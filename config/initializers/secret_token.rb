c2df8309096284b7cfde7c43334686a24c62930f
Howard Ding <hding2@illinois.edu>
2012-02-16 16:25:54 -0600

    Initial add - This will be an attempt to convert our original medusa rails project (based on older code and largely experimental) to a project based on newer code that will be used to ingest content into our production server.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MedusaRails3::Application.config.secret_token = 'c797f55dba5defd0d62793942040cce877ecafb5c85634b616f734e62e05587fb1cb9acba9e86c557148a0f163e112f7ee256ad6a208d3d4dfa336d48f088be4'
