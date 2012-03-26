745c7540d46b90aba35300bab28a81a60e32eb38
Howard Ding <hding2@illinois.edu>
2012-03-26 12:17:06 -0500

    add relationships so that a set can have subsets and so that it recursively deletes them correctly

--- app/models/medusa/set.rb
+++ app/models/medusa/set.rb
@@ -1,10 +1,14 @@
 module Medusa
   class Set < ActiveFedora::Base
     has_relationship "members", :is_member_of, :inbound => true
+    has_relationship "subset_of", :is_subset_of
+    has_relationship "subsets", :is_subset_of, :inbound => true
 
     def recursive_delete
+      self.subsets.each {|subset| subset.recursive_delete}
       self.members.each {|member| member.recursive_delete}
       self.delete
     end
   end
+
 end
