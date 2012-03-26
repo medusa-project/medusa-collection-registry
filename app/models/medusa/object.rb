97dd95276d0f2446e8f05f325914fa0cea50fc17
Howard Ding <hding2@illinois.edu>
2012-03-26 16:20:31 -0500

    Add method that gives url to the object.

--- app/models/medusa/object.rb
+++ app/models/medusa/object.rb
@@ -4,5 +4,10 @@ module Medusa
       puts "DELETING class: #{self.class.to_s} pid: #{self.pid}"
       self.delete
     end
+
+    #use the fedora config to generate a url where this object can be accessed
+    def url
+      "#{ActiveFedora.fedora_config[Rails.env.to_sym][:url]}/objects/#{self.pid}"
+    end
   end
 end
