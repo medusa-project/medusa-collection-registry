512d7034edbf97b0fbab50cd63faecbeedd9bd78
Howard Ding <hding2@illinois.edu>
2012-03-29 10:42:34 -0500

    bit of refactoring - extract a couple methods to isolate their functionality

--- app/models/medusa/mods_to_dublin_core_converter.rb
+++ app/models/medusa/mods_to_dublin_core_converter.rb
@@ -24,15 +24,25 @@ module Medusa
     #take the mods file, use an xslt to convert to dc, add the object pid as a dc:identifier to the
     #dc, and if there is a dc:title then use it as the label of the object
     def dc_from_mods_file_and_object(filename, fedora_object)
-      dc = self.transform_mods_file(filename)
-      #now we need to add a dc:identifier with the pid of the object - this is a bit complex because of the namespaces
-      id_node = Nokogiri::XML::Node.new('dc:identifier', dc)
-      id_node.add_namespace(nil, dc.namespaces['xmlns:dc'])
+      self.transform_mods_file(filename).tap do |dc|
+        add_pid_to_dc(dc, fedora_object)
+        add_label_to_object(dc, fedora_object)
+      end
+    end
+
+    protected
+
+    #This is a bit complex because of the namespaces, but this is how you handle it.
+    def add_pid_to_dc(dc_doc, fedora_object)
+      id_node = Nokogiri::XML::Node.new('dc:identifier', dc_doc)
+      id_node.add_namespace(nil, dc_doc.namespaces['xmlns:dc'])
       id_node.content = fedora_object.pid
-      dc.root.add_child(id_node)
-      title = dc.at_xpath('//dc:title')
+      dc_doc.root.add_child(id_node)
+    end
+
+    def add_label_to_object(dc_doc, fedora_object)
+      title = dc_doc.at_xpath('//dc:title')
       fedora_object.label = title.text if title
-      dc
     end
 
   end
