cddec10849751544c5a71910af53135fb6d8bade
Howard Ding <hding2@illinois.edu>
2012-08-08 13:33:14 -0500

    Move the code to generate the link into a helper function and make it behave well when the net_id is blank.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def net_id_search_url(net_id)
     "http://illinois.edu/ds/search?skinId=0&search_type=userid&search=#{net_id}"
   end
+
+  def net_id_search_link(net_id)
+    if net_id.blank?
+      ''
+    else
+      link_to net_id, net_id_search_url(net_id), :target => '_blank', :class => 'net-id-search'
+    end
+  end
 end
