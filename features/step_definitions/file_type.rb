271ed86f978f2d962af8500c36767d290ad9f319
Howard Ding <hding2@illinois.edu>
2012-08-31 13:03:16 -0500

    Fix typo.

--- features/step_definitions/file_type.rb
+++ features/step_definitions/file_type.rb
@@ -1,5 +1,5 @@
 Then /^There should be standard default file types$/ do
-   ['Derivatgit ive Content','Master Metadata','Other'].each do |name|
+   ['Derivative Content','Master Metadata','Other'].each do |name|
     FileType.find_by_name(name).should_not be_nil
   end
 end
