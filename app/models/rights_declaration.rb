7b45236f6bf14355380b2af388bd108494f92570
Howard Ding <hding2@illinois.edu>
2012-11-07 18:00:29 -0600

    Have loading of rights fields from config now.

--- app/models/rights_declaration.rb
+++ app/models/rights_declaration.rb
@@ -6,21 +6,16 @@ class RightsDeclaration < ActiveRecord::Base
                  :copyright_statements, :default_copyright_statement, :access_restrictions,
                  :default_access_restrictions, :instance_accessor => false
 
-  #initialization - called when loading class
+  #initialization
   def self.load_rights_data
-    self.rights_bases = %w(copyright statute license other)
-    self.default_rights_basis = 'copyright'
-    self.copyright_jurisdictions = {'us' => 'United States',
-                                    'xxc' => 'Canada'}
-    self.default_copyright_jurisdiction = 'us'
-    self.copyright_statements ={'pd' => 'Public domain.',
-                                'us' => 'Public domain. U.S. Government document.'}
-    self.default_copyright_statement = 'pd'
-    self.access_restrictions = {'DISSEMINATE' => 'Access is open and unrestricted.',
-                                'DISSEMINATE/DISALLOW' => 'Access is restricted.'}
-    self.default_access_restrictions = 'DISSEMINATE/DISALLOW'
+    rights_yaml = YAML.load_file(File.join(Rails.root, 'config', 'rights_fields.yml'))
+    %w(rights_bases default_rights_basis copyright_jurisdictions default_copyright_jurisdiction
+copyright_statements default_copyright_statement access_restrictions default_access_restrictions).each do |field|
+      self.send("#{field}=", rights_yaml[field])
+    end
   end
 
+  #we need to call this as we're loading the class so that the validations for inclusion will work properly
   load_rights_data
 
   def set_defaults
@@ -29,10 +24,10 @@ class RightsDeclaration < ActiveRecord::Base
     self.access_restrictions ||= self.class.default_access_restrictions
   end
 
-  validates_inclusion_of :rights_basis, :in => self.rights_bases
-  validates_inclusion_of :copyright_jurisdiction, :in => self.copyright_jurisdictions.keys
-  validates_inclusion_of :copyright_statement, :in => self.copyright_statements.keys, :allow_blank => true
-  validates_inclusion_of :access_restrictions, :in => self.access_restrictions.keys
+  validates_inclusion_of :rights_basis, :in => Proc.new{self.rights_bases}
+  validates_inclusion_of :copyright_jurisdiction, :in => Proc.new{self.copyright_jurisdictions.keys}
+  validates_inclusion_of :copyright_statement, :in => Proc.new{self.copyright_statements.keys}, :allow_blank => true
+  validates_inclusion_of :access_restrictions, :in => Proc.new{self.access_restrictions.keys}
 
   def copyright_jurisdiction_text
     self.class.copyright_jurisdictions[self.copyright_jurisdiction]
