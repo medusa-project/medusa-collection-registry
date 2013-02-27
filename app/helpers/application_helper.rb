64e7affc32a821746035548b9492353f1eaec84b
Howard Ding <hding2@illinois.edu>
2013-02-27 12:51:21 -0600

    Refactored some of the boilerplate around making a date picker field. Unfortunately it doesn't seem as easy to do this with a custom simple_form control as I might have liked. Someone prove me wrong.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -50,4 +50,9 @@ module ApplicationHelper
     link_to label, 'https://wiki.cites.uiuc.edu/wiki/display/LibraryDigitalPreservation/Home', :target => '_blank'
   end
 
+  def date_picker_options(extra_opts = {})
+    {:as => :string, :input_html => {'data-datepicker' => 'datepicker'},
+     :order => [:day, :month, :year], :use_month_numbers => true}.merge(extra_opts)
+  end
+
 end
