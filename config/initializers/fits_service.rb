06c6e21766c958017970877368109bab7c12183e
Howard Ding <hding2@illinois.edu>
2013-03-05 13:30:26 -0600

    Fix configuration - need to actually load file.

--- config/initializers/fits_service.rb
+++ config/initializers/fits_service.rb
@@ -1,4 +1,4 @@
 require 'fits'
 
-config = File.join(Rails.root, 'config', 'fits_service.yml')[Rails.env]
+config = YAML.load_file(File.join(Rails.root, 'config', 'fits_service.yml'))[Rails.env]
 Fits::Service.instance.configure(config) unless Rails.env == 'test'
