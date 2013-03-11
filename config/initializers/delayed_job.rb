d400c214198121a215b0ed76cd7c28cc63503e88
Howard Ding <hding2@illinois.edu>
2013-03-11 10:38:55 -0500

    typo

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -3,5 +3,5 @@ Delayed::Worker.delay_jobs = !Rails.env.test?
 
 #don't run all that often in development - can always run them off manually if needed
 if Rails.env.development?
-  Delay::Worker.sleep_delay = 600
+  Delayed::Worker.sleep_delay = 600
 end
