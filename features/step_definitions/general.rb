4789d7f00ec9db337cadadcb7204ad72dc5b58fe
= <hading2@gmail.com>
2013-03-25 17:11:15 -0500

    add comment

--- features/step_definitions/general.rb
+++ features/step_definitions/general.rb
@@ -8,5 +8,6 @@ end
 
 #TODO - this doesn't work as is, and I'm not sure it is generally possible via Capybara
 Then(/^the response code should be (\d+)$/) do |code|
+  pending
   response.status.to_s.should == code
 end
