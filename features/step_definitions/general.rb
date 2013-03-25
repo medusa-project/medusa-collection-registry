f70aaa3fa900318ebca151aee0ac6c0a6b08eeb0
= <hading2@gmail.com>
2013-03-25 15:45:29 -0500

    Add comment

--- features/step_definitions/general.rb
+++ features/step_definitions/general.rb
@@ -6,6 +6,7 @@ Given /^Nothing$/ do
   #do nothing - just to explicitly say that no Given is really required
 end
 
+#TODO - this doesn't work as is, and I'm not sure it is generally possible via Capybara
 Then(/^the response code should be (\d+)$/) do |code|
-  page.response.code.to_s.should == code
+  response.status.to_s.should == code
 end
