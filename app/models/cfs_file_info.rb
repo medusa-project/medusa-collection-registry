64f8116b6f5345050b2f814ffda659db8886977d
Howard Ding <hding2@illinois.edu>
2013-04-08 13:03:43 -0500

    removed unused code

--- app/models/cfs_file_info.rb
+++ app/models/cfs_file_info.rb
@@ -3,14 +3,4 @@ class CfsFileInfo < ActiveRecord::Base
 
   validates_uniqueness_of :path, :allow_blank => false
 
-  after_create :schedule_basic_assessment
-
-  def schedule_basic_assessment
-    self.delay.basic_assess
-  end
-
-  def basic_assess
-
-  end
-
 end
