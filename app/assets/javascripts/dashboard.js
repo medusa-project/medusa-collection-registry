f7dcb90c32e71eea9164b99fdc2f7cc99b2ee3a0
Howard Ding <hding2@illinois.edu>
2013-04-10 10:56:03 -0500

    Extract storage overview table into partial. Fix double use of html id Add file for dashboard css.

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,86 +1,90 @@
 initialize_data_table("table#file_stats_bits");
 initialize_data_table("table#file_stats_objects");
-initialize_data_table("table#red_flags")
+initialize_data_table("table#red_flags_table");
 
 function initializeChart(storage) {
-        $('.chart_container').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false
-            },
-            title: {
-                text: 'size(KB)'
-            },
-            tooltip: {
-        	    pointFormat: '{series.name}: <b>{point.percentage}%</b>',
-            	percentageDecimals: 1
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: true,
-                        color: '#000000',
-                        connectorColor: '#000000',
-                        formatter: function() {
-                            return '<b>'+ this.point.name +'</b>';
-                        }
-                    }
-                }
-            },
-            series: [{
-                type: 'pie',
-                name: 'Size share',
-                data: [
-                    ['Object Level', storage["object_level_total"]/storage["total"]],
-                    {
-                        name: 'Bit Level',
-                        y: storage["bit_level_total"]/storage["total"],
-                        sliced: false,
-                        selected: true
-                    },
-                    ['Free space', storage["free"]/storage["total"] ],
-                ]}]});
-        $('#chart_container2').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false
-            },
-            title: {
-                text: 'size(KB)'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage}%</b>',
-                percentageDecimals: 1
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: true,
-                        color: '#000000',
-                        connectorColor: '#000000',
-                        formatter: function() {
-                            return '<b>'+ this.point.name +'</b>';
-                        }
-                    }
-                }
-            },
-            series: [{
-                type: 'pie',
-                name: 'Size share',
-                data: [
-                    ['Object Level',   25.0],
-                    {
-                        name: 'Bit Level',
-                        y: 50.0,
-                        sliced: false,
-                        selected: true
-                    },
-                    ['Free space',    25.0],
-                ]}]});
-    }
+  $('.chart_container').highcharts({
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false
+    },
+    title: {
+      text: 'size(KB)'
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+      percentageDecimals: 1
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          color: '#000000',
+          connectorColor: '#000000',
+          formatter: function () {
+            return '<b>' + this.point.name + '</b>';
+          }
+        }
+      }
+    },
+    series: [
+      {
+        type: 'pie',
+        name: 'Size share',
+        data: [
+          ['Object Level', storage["object_level_total"] / storage["total"]],
+          {
+            name: 'Bit Level',
+            y: storage["bit_level_total"] / storage["total"],
+            sliced: false,
+            selected: true
+          },
+          ['Free space', storage["free"] / storage["total"] ],
+        ]}
+    ]});
+  $('#chart_container2').highcharts({
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false
+    },
+    title: {
+      text: 'size(KB)'
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+      percentageDecimals: 1
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          color: '#000000',
+          connectorColor: '#000000',
+          formatter: function () {
+            return '<b>' + this.point.name + '</b>';
+          }
+        }
+      }
+    },
+    series: [
+      {
+        type: 'pie',
+        name: 'Size share',
+        data: [
+          ['Object Level', 25.0],
+          {
+            name: 'Bit Level',
+            y: 50.0,
+            sliced: false,
+            selected: true
+          },
+          ['Free space', 25.0],
+        ]}
+    ]});
+}
