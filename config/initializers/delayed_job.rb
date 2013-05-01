226fc34ed48c08403047419623b15316df41a7a8
Howard Ding <hding2@illinois.edu>
2013-05-01 16:46:20 -0500

    Add line to be used when we don't want to delay in development.

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,5 +1,7 @@
-#don't delay jobs in test environment
+#don't delay jobs in test environment - sometimes we may want to also not delay in development for debugging -
+#use second line for this
 Delayed::Worker.delay_jobs = !Rails.env.test?
+#Delayed::Worker.delay_jobs = false
 
 #don't run all that often in development - can always run them off manually if needed
 if Rails.env.development?
