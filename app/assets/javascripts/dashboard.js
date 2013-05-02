666fe80fb5498cfb4a0e1765d56388791c7e0892
Howard Ding <hding2@illinois.edu>
2013-05-02 15:55:58 -0500

    removed old 'other storage target' view and javascript code

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -50,47 +50,5 @@ function initializeChart(storage) {
           },
           ['Free space', storage["free"] / storage["total"] ],
         ]}
-    ]});
-  $('#chart_container2').highcharts({
-    chart: {
-      plotBackgroundColor: null,
-      plotBorderWidth: null,
-      plotShadow: false
-    },
-    title: {
-      text: 'size(KB)'
-    },
-    tooltip: {
-      pointFormat: '{series.name}: <b>{point.percentage}%</b>',
-      percentageDecimals: 1
-    },
-    plotOptions: {
-      pie: {
-        allowPointSelect: true,
-        cursor: 'pointer',
-        dataLabels: {
-          enabled: true,
-          color: '#000000',
-          connectorColor: '#000000',
-          formatter: function () {
-            return '<b>' + this.point.name + '</b>';
-          }
-        }
-      }
-    },
-    series: [
-      {
-        type: 'pie',
-        name: 'Size share',
-        data: [
-          ['Object Level', 25.0],
-          {
-            name: 'Bit Level',
-            y: 50.0,
-            sliced: false,
-            selected: true
-          },
-          ['Free space', 25.0],
-        ]}
-    ]});
-}
+    ]})
+};
