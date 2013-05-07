a95922f35ec96a50559997fe7c608b5ff4d45f35
Nikhil Tayal <nikhiltayal86@gmail.com>
2013-05-07 10:46:24 -0500

    Creating Polymorphic attachments and adding it to collections

--- app/assets/javascripts/collections.js
+++ app/assets/javascripts/collections.js
@@ -10,3 +10,4 @@ $(function () {
 initialize_data_table("table#collections")
 initialize_data_table("table#assessments")
 initialize_data_table("table#file_groups")
+initialize_data_table("table#attachments")
