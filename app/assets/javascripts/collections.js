a4ac315efebf9310367094e35e4e9e13cef75719
Howard Ding <hding2@illinois.edu>
2013-05-30 11:14:58 -0500

    Add javascript to filter on storage levels. Add view links to trigger javascript.

--- app/assets/javascripts/collections.js
+++ app/assets/javascripts/collections.js
@@ -1,4 +1,26 @@
-initialize_data_table("table#collections")
-initialize_data_table("table#assessments")
-initialize_data_table("table#file_groups")
-initialize_data_table("table#attachments")
+initialize_data_table("table#collections");
+initialize_data_table("table#assessments");
+initialize_data_table("table#file_groups");
+initialize_data_table("table#attachments");
+
+var storage_level_filter = {
+  all: function () {
+    this.filter('');
+  },
+
+  external: function () {
+    this.filter('external');
+  },
+
+  bit_level: function () {
+    this.filter('bit-level store');
+  },
+
+  object_level: function() {
+    this.filter('object-level store');
+  },
+
+  filter: function(filter_string) {
+    $('#file_groups').dataTable().fnFilter(filter_string, 2)
+  }
+};
