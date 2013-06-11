bfc57364dc27f7aa39427196c289c35d9ac68a23
Howard Ding <hding2@illinois.edu>
2013-06-11 13:51:56 -0500

    Fix things up so the test environment will continue to use the :test mail delivery but still be able to generate urls in the mailer.

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,6 +1,8 @@
 config_hash = YAML.load_file(File.join(Rails.root, 'config', 'smtp.yml'))[Rails.env]
 if config_hash
-  ActionMailer::Base.delivery_method = :smtp
+  unless Rails.env.test?
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = config_hash['smtp_settings']
+  end
   ActionMailer::Base.default_url_options = {:host => config_hash['web_host']}
-  ActionMailer::Base.smtp_settings = config_hash['smtp_settings']
 end
