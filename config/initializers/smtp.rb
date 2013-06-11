43dbe2823411f013059fb71f1aee1f38d7f013a3
Howard Ding <hding2@illinois.edu>
2013-06-11 12:19:36 -0500

    Guard against missing section.

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,6 +1,8 @@
 config_hash = YAML.load_file(File.join(Rails.root, 'config', 'smtp.yml'))[Rails.env]
-MedusaRails3::Application.config do |config|
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = config_hash['smtp_settings']
-  config.action_mailer.default_url_options = {:host => config_hash['web_host']}
+if config_hash
+  MedusaRails3::Application.config do |config|
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = config_hash['smtp_settings']
+    config.action_mailer.default_url_options = {:host => config_hash['web_host']}
+  end
 end
