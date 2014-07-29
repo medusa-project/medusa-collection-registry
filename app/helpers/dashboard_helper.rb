d4e4d98313e5de3340c3360f699586176213a752
Howard Ding <hding2@illinois.edu>
2014-07-29 18:04:05 -0500

    Optimize query for file stats on dashboard.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,29 +1,19 @@
 module DashboardHelper
   include ActionView::Helpers::NumberHelper
+
   def dashboard_tab_list
     ['storage-overview', 'running-processes', 'file-statistics', 'red-flags', ['combined-events-tab', 'Events'], 'amazon']
   end
 
   # File Stats for bit preservation
-
-  #According to the Rails docs this should be possible by
-  #CfsFile.distinct.pluck(:content_type), but I couldn't get that working.
-  #Perhaps it's Rails4? Regardless that would be better because it'd be a lot
-  #more efficient - if that turns into a problem research some more, or even
-  #just use SQL directly for this.
-  def list_of_bit_file_formats
-    # return list of unique content types
-    CfsFile.where('content_type IS NOT NULL').pluck(:content_type).uniq.sort
-  end
-
-  def size_bits_type_format (content_type)
-    # only active records of certain format that have been ingested
-    CfsFile.where(:content_type => content_type).sum(:size)
-  end
-
-  def file_count_bits_type_format (content_type)
-    # only active records of certain format that have been ingested
-    CfsFile.where(:content_type => content_type).count
+  #return a hash with content_type => hash(total_size, count)
+  def bit_file_info_summary
+    Hash.new.tap do |h|
+      CfsFile.select('content_type, sum(size) as total_size, count(*) as count').group('content_type').order('content_type').each do |record|
+        content_type = record[:content_type] || 'Unknown'
+        h[content_type] = {:size => record[:total_size] || 0, :count => record[:count] || 0}
+      end
+    end
   end
 
   def size_bits_total
@@ -43,13 +33,13 @@ module DashboardHelper
   end
 
   def size_objects_type_format (ct)
-     # only active records of certain format that have been ingested
-     number_with_precision(0.0, :precision => 4)
+    # only active records of certain format that have been ingested
+    number_with_precision(0.0, :precision => 4)
   end
 
   def file_count_objects_type_format (ct)
-     # only active records of certain format that have been ingested
-     0
+    # only active records of certain format that have been ingested
+    0
   end
 
   def size_objects_total
