eca147564853a90d434e0226d9f24a7417ad50f2
Howard Ding <hding2@illinois.edu>
2014-09-19 15:05:42 -0500

    Add relationships to the base classes for workflows and jobs. Will need some concrete classes to make sure that these are right, but it's a start.

--- app/models/workflow/base.rb
+++ app/models/workflow/base.rb
@@ -1,5 +1,7 @@
 class Workflow::Base < ActiveRecord::Base
 
   self.abstract_class = true
+  has_one :workflow_workflow_job_relation, :class_name => 'Workflow::WorkflowJobRelation', as: :workflow
+  has_one :job, through: :workflow_workflow_job_relation
 
 end
--- app/models/workflow/base_job.rb
+++ app/models/workflow/base_job.rb
@@ -1,5 +1,7 @@
 class Workflow::BaseJob < ActiveRecord::Base
 
   self.abstract_class = true
+  has_one :workflow_workflow_job_relation, :class_name => 'Workflow::WorkflowJobRelation', as: :job
+  has_one :workflow, through: :workflow_workflow_job_relation
 
 end
