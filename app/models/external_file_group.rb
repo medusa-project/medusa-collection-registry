2ffcc1746abd2d86b5e7b3ea936600858c38a66f
Howard Ding <hding2@illinois.edu>
2014-10-03 17:53:44 -0500

    More code for figuring out if staged directory exists

--- app/models/external_file_group.rb
+++ app/models/external_file_group.rb
@@ -10,10 +10,13 @@ class ExternalFileGroup < FileGroup
     ['BitLevelFileGroup', 'ObjectLevelFileGroup']
   end
 
+  #check that the staged_file_location is okay - it must end in collection_id/file_group_id
+  #check that the staged storage has a matching root
+  #check that the corresponding staged storage local directory exists
   def has_staged_directory?
-    #check that the staged_file_location is okay - it must end in collection_id/file_group_id
-    #check that the staged storage has a matching root
-    #check that the corresponding staged storage local directory exists
+    self.staged_file_location and
+        StagingStorage.normalize_path(self.staged_file_location).match(/#{self.collection_id}\/#{self.id}$/) and
+        StagingStorage.root_for(self.staged_file_location)
   end
 
   def ready_for_bit_level_ingest?
