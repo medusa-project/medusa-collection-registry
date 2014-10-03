0f1627ddef82e1c96bb884cac3e8421427145089
Howard Ding <hding2@illinois.edu>
2014-10-03 17:50:00 -0500

    Add utility method for normalizing paths

--- app/models/staging_storage.rb
+++ app/models/staging_storage.rb
@@ -10,4 +10,9 @@ class StagingStorage < Object
     @roots = config_roots.collect {|root_hash| StagingStorageRoot.new(root_hash)}
   end
 
+  #return the path with all '\' changed to '/' and any multiples condensed to one
+  def self.normalize_path(path)
+    path.tr('\\', '/').gsub(/\/+/, '/')
+  end
+
 end
