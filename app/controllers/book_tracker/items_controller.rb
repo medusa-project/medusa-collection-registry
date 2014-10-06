0cfe338d23da1281eeb09a5f29639a3460b26654
Alex Dolski <alexd@n-ary.net>
2014-10-06 11:36:48 -0500

    Add live search to items view

--- app/controllers/book_tracker/items_controller.rb
+++ app/controllers/book_tracker/items_controller.rb
@@ -3,7 +3,12 @@ module BookTracker
   class ItemsController < ApplicationController
 
     def index
-      @items = Item.all.paginate(page: params[:page], per_page: 100)
+      q = "%#{params[:q]}%"
+      @items = Item.where('CAST(bib_id AS VARCHAR(10)) LIKE ? '\
+        'OR oclc_number LIKE ? OR obj_id LIKE ? OR LOWER(title) LIKE LOWER(?) '\
+        'OR LOWER(author) LIKE LOWER(?) OR LOWER(ia_identifier) LIKE LOWER(?)',
+        q, q, q, q, q, q).
+          order(:title).paginate(page: params[:page], per_page: 100)
     end
 
     def show
