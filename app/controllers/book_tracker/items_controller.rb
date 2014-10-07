40053e19ba0a262c60bf1042efb2bd92575a9936
Alex Dolski <alexd@n-ary.net>
2014-10-07 11:48:10 -0500

    Include date in filtering

--- app/controllers/book_tracker/items_controller.rb
+++ app/controllers/book_tracker/items_controller.rb
@@ -7,8 +7,9 @@ module BookTracker
       @items = Item.all
       @items = @items.where('CAST(bib_id AS VARCHAR(10)) LIKE ? '\
         'OR oclc_number LIKE ? OR obj_id LIKE ? OR LOWER(title) LIKE LOWER(?) '\
-        'OR LOWER(author) LIKE LOWER(?) OR LOWER(ia_identifier) LIKE LOWER(?)',
-        q, q, q, q, q, q) unless params[:q].blank?
+        'OR LOWER(author) LIKE LOWER(?) OR LOWER(ia_identifier) LIKE LOWER(?)' \
+        'OR LOWER(date) LIKE LOWER(?)',
+        q, q, q, q, q, q, q) unless params[:q].blank?
       @items = @items.where(exists_in_hathitrust: params[:ht]) unless params[:ht].blank?
       @items = @items.where(exists_in_internet_archive: params[:ia]) unless params[:ia].blank?
       @items = @items.order(:title)
