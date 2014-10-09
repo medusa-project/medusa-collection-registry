516e5a67e76d661a44e18e3f98e6022c98bb9c74
Howard Ding <hding2@illinois.edu>
2014-10-09 13:12:11 -0500

    Add comment describing how this class will work.

--- app/models/workflow/ingest.rb
+++ app/models/workflow/ingest.rb
@@ -1,3 +1,7 @@
+# Create (in start state) and enqueue
+# - change to copying state, copy directory, reenqueue
+# - change to backing up state, register amazon backup, do not reenqueue
+# - when backup is complete, it will call back to finish process
 class Workflow::Ingest < Workflow::Base
   belongs_to :external_file_group
   belongs_to :bit_level_file_group
