761bc34de91f76ede12378dbd44be6debd9964a7
Alex Dolski <alexd@n-ary.net>
2014-10-31 15:09:00 -0500

    Improve Google query construction

--- app/models/book_tracker/item.rb
+++ app/models/book_tracker/item.rb
@@ -114,9 +114,10 @@ module BookTracker
     # @return string
     #
     def google_url
-      disallowed_characters = '`~!@#$%^&*()_\+\-=[]{}|\\\"\'<>,.?/:;'
-      sanitized_title = self.title.tr(disallowed_characters, '')
-      sanitized_author = self.author.tr(disallowed_characters, '')
+      strip_characters = '`~!@#$%^&*()\\=[]{}|\\\"\'<>,.?/:;'
+      blank_characters = '-_+'
+      sanitized_title = self.title.tr(strip_characters, '').tr(blank_characters, ' ')
+      sanitized_author = self.author.tr(strip_characters, '').tr(blank_characters, ' ')
       q = []
       q << 'intitle:' + sanitized_title.split(' ').select{ |t| t.length > 1 }.
           join(' intitle:') unless sanitized_title.blank?
