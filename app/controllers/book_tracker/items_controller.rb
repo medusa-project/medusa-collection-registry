bc23444a3987283d5b87b48ed416faef5b52f74f
Alex Dolski <alexd@n-ary.net>
2014-11-06 09:39:28 -0600

    Change XLS output to CSV (MED-130)

--- app/controllers/book_tracker/items_controller.rb
+++ app/controllers/book_tracker/items_controller.rb
@@ -82,15 +82,15 @@ module BookTracker
         format.html {
           @items = @items.paginate(page: params[:page], per_page: 100)
         }
-        format.csv { send_data @items.to_csv }
+        format.csv {
+          items_csv = @items.to_csv
+          @missing_bib_ids.each do |id|
+            items_csv << id
+          end
+          send_data(items_csv)
+        }
         format.js {
           @items = @items.paginate(page: params[:page], per_page: 100)
-        }
-        format.xls {
-          items_csv = @items.to_csv(col_sep: "\t")
-          num_cols = items_csv.split("\n")[0].split("\t").length
-          send_data(items_csv + @missing_bib_ids.
-              map{ |id| id + ("\t" * (num_cols - 1)) }.join("\n"))
         }
       end
     end
