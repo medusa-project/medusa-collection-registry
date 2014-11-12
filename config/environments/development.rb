190cf8705459820cd18f83845cf2630df5bbc0f0
Howard Ding <hding2@illinois.edu>
2014-11-12 17:00:05 -0600

    Add line to make it easy to switch caching behavior.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,7 @@ MedusaRails3::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  #config.action_controller.perform_caching = true
 
   #to test caching uncomment the following
   #config.action_controller.perform_caching = true
