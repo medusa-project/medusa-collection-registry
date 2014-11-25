d01fe894a4f204f0732bda2c3f5c27cb76a76026
Bill Ingram <waingram@gmail.com>
2014-11-25 16:30:35 -0600

    upgrade twitter-bootstrap-rails

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module MedusaRails3
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.2'
 
   end
 end
