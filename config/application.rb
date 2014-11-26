53a5f2ce95386935bafc28d93040b6770c8123cd
Bill Ingram <waingram@gmail.com>
2014-11-26 11:28:23 -0600

    Update the plataformatec/simple_form gem from 3.1.0.rc2 to the official release 3.1.0. Also remove the github reference because 3.1.0 is the version in rubygems.

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module MedusaRails3
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.2'
+    config.assets.version = '1.3'
 
   end
 end
