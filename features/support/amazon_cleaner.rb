15249242b1f3b01bdb6bbada714983e267beeda5
Howard Ding <hding2@illinois.edu>
2014-12-08 15:43:39 -0600

    no longer need to delete amazon bag stuff

--- features/support/amazon_cleaner.rb
+++ features/support/amazon_cleaner.rb
@@ -1,10 +1,7 @@
 require 'fileutils'
 
-#before each test make sure that the amazon bag directory and AMQP queues are clean
+#before each test make sure that the amazon AMQP queues are clean
 Before do
-  Dir[File.join(AmazonBackup.storage_root, '*')].each do |dir|
-    FileUtils.rm_rf(dir)
-  end
   Test::AmazonGlacierServer.instance.clear_queues
 end
 
