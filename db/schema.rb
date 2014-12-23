41520bbeebf9d54d51337b8aa986507661870b92
Alex Dolski <alexd@n-ary.net>
2014-12-23 16:57:01 -0600

    Add book_tracker_items.source_path

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141222162152) do
+ActiveRecord::Schema.define(version: 20141223214209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20141222162152) do
     t.boolean  "exists_in_internet_archive", default: false
     t.text     "raw_marcxml"
     t.boolean  "exists_in_google",           default: false
+    t.text     "source_path"
   end
 
   add_index "book_tracker_items", ["author"], name: "index_book_tracker_items_on_author", using: :btree
