7c096e1419705dc84e51d99b9a8d0ab2a523c378
Howard Ding <hding2@illinois.edu>
2015-01-27 11:27:16 -0600

    formatting

--- features/step_definitions/events.rb
+++ features/step_definitions/events.rb
@@ -1,6 +1,5 @@
 And /^the (.*) with (.*) '(.*)' has an event with key '(.*)' performed by '(.*)'$/ do |object_type, key, value, event_key, email|
-  object = find_or_create_object(object_type, key, value)
-  object.events.create(key: event_key, actor_email: email, date: Date.today)
+  find_or_create_object(object_type, key, value).events.create(key: event_key, actor_email: email, date: Date.today)
 end
 
 Then /^the (.*) with (.*) '(.*)' should have an event with key '(.*)' performed by '(.*)'$/ do |object_type, key, value, event_key, email|
@@ -25,9 +24,9 @@ Then(/^the (.*) with (.*) '(.*)' should have cascadable events with fields:$/) d
   object = find_object(object_type, key, value)
   table.hashes.each do |hash|
     object.cascaded_events.detect do |event|
-        hash.keys.all? do |hash_key|
-          event.send(hash_key) == hash[hash_key]
-        end
+      hash.keys.all? do |hash_key|
+        event.send(hash_key) == hash[hash_key]
+      end
     end.should be_truthy
   end
 end
@@ -42,12 +41,12 @@ end
 
 Then /^a (.*) is unauthorized to create an event for the file group titled '(.*)'$/ do |user_type, title|
   if user_type == 'visitor'
-      rack_login('a visitor')
-      expected_path = unauthorized_path
-    else
-      expected_path = login_path
+    rack_login('a visitor')
+    expected_path = unauthorized_path
+  else
+    expected_path = login_path
   end
   post events_path(eventable_type: 'FileGroup', eventable_id: FileGroup.find_by(title: title))
   expect(last_response.redirect?).to be_truthy
-    expect(last_response.location).to match(/#{expected_path}$/)
+  expect(last_response.location).to match(/#{expected_path}$/)
 end
