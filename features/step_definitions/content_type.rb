1fcd86199e7dac7cb192fff8884d3c39422357d8
Howard Ding <hding2@illinois.edu>
2015-01-28 16:22:26 -0600

    remove unused step

--- features/step_definitions/content_type.rb
+++ features/step_definitions/content_type.rb
@@ -1,7 +1,3 @@
 Then(/^I should be on the cfs files page for the (.*) with (.*) '(.*)'$/) do |object_type, key, value|
   expect(current_path).to eq(specific_object_path(object_type, key, value, 'cfs_files'))
 end
-
-And(/^I view cfs files for the content type with name '(.*)'$/) do |content_type_name|
-  visit (specific_object_path('content type', 'name', content_type_name, 'cfs_files'))
-end
