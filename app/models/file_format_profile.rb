ac8b45ac6257bf8466bd628f63022695842cf324
Howard Ding <hding2@illinois.edu>
2015-01-29 17:36:26 -0600

    Make the content types and extensions order more logically. Make the labels strong

--- app/models/file_format_profile.rb
+++ app/models/file_format_profile.rb
@@ -2,8 +2,8 @@ class FileFormatProfile < ActiveRecord::Base
 
   validates_uniqueness_of :name, allow_blank: false
   has_many :file_format_profiles_content_types_joins, dependent: :destroy
-  has_many :content_types, through: :file_format_profiles_content_types_joins
+  has_many :content_types, -> {order "name asc"}, through: :file_format_profiles_content_types_joins
   has_many :file_format_profiles_file_extensions_joins, dependent: :destroy
-  has_many :file_extensions, through: :file_format_profiles_file_extensions_joins
+  has_many :file_extensions, -> {order "extension asc"}, through: :file_format_profiles_file_extensions_joins
 
 end
