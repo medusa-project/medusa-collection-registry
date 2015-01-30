9ad5f31d62f72015ad677aaaafccfc2b5082e024
Howard Ding <hding2@illinois.edu>
2015-01-30 12:39:34 -0600

    simplify ids

--- app/assets/javascripts/file_extension.js
+++ app/assets/javascripts/file_extension.js
@@ -1,4 +1,4 @@
 $(function() {
-  $("#file_format_profiles_edit_content_type_list span").addClass("col-sm-6");
-  $("#file_format_profiles_edit_file_extension_list span").addClass("col-sm-3");
+  $("#edit_content_type_list span").addClass("col-sm-6");
+  $("#edit_file_extension_list span").addClass("col-sm-3");
 });
