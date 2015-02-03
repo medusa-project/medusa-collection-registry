d8a2b3d9ee8bf29371212aed244815770f1c4561
Howard Ding <hding2@illinois.edu>
2015-02-03 11:08:19 -0600

    Update git repo name and checkout location

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ task :staging do
 end
 
 set :application, "Medusa"
-set :repository, "git://github.com/medusa-project/medusa-rails3.git"
+set :repository, "git://github.com/medusa-project/medusa-collection-registry.git"
 
 set :scm, :git
 set :deploy_via, :remote_cache
@@ -28,7 +28,7 @@ set :user, 'medusa'
 set :use_sudo, false
 
 set :home, "/services/medusa"
-set :deploy_to, "#{home}/medusa-rails3-capistrano"
+set :deploy_to, "#{home}/medusa-collection-registry-capistrano"
 set :shared, "#{deploy_to}/shared"
 set :shared_config, "#{shared}/config"
 set :public, "#{current_path}/public"
