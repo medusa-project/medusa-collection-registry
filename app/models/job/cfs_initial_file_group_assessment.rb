b22ec8facc0aa04aa15ad6d94b63e568b0df14c1
Howard Ding <hding2@illinois.edu>
2015-02-19 12:25:30 -0600

    Add some error checking.

--- app/models/job/cfs_initial_file_group_assessment.rb
+++ app/models/job/cfs_initial_file_group_assessment.rb
@@ -2,10 +2,12 @@ class Job::CfsInitialFileGroupAssessment < Job::Base
   belongs_to :file_group, touch: true
 
   def self.create_for(file_group)
+    raise RuntimeError, "No cfs directory defined for file group #{file_group.id}. Cannot create assessment job." unless file_group.cfs_directory.present?
     Delayed::Job.enqueue(self.create!(file_group: file_group), priority: 60, queue: 'initial_assessment') unless self.find_by(file_group_id: file_group.id)
   end
 
   def perform
+    raise RuntimeError, "No cfs directory defined for file group #{self.file_group.id}. Cannot run assessment job." unless self.file_group.cfs_directory.present?
     self.file_group.run_initial_cfs_assessment
   end
 
