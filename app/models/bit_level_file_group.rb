82bfbb5f2eb274badbef2870735759a3d24fa23a
Howard Ding <hding2@illinois.edu>
2015-03-06 14:25:22 -0600

    Use SQL to get less object creation.

--- app/models/bit_level_file_group.rb
+++ app/models/bit_level_file_group.rb
@@ -155,4 +155,57 @@ class BitLevelFileGroup < FileGroup
     self.cfs_directory(true)
   end
 
+  #return a hash with key the id
+  #values are hashes with title, collection id and title, repository id and title, total file size and count,
+  #last backup date (or nil) and last backup completed (or nil)
+  def self.dashboard_amazon_info
+    backup_info_hash = file_group_latest_amazon_backup_hash
+    file_group_info_query = <<SQL
+    SELECT FG.id, FG.title, FG.total_files, FG.total_file_size, C.id AS collection_id, C.title AS collection_title,
+           R.id AS repository_id, R.title AS repository_title
+    FROM file_groups FG, collections C, repositories R, cfs_directories CFS
+    WHERE FG.type = 'BitLevelFileGroup' AND FG.collection_id = C.id AND c.repository_id = R.id
+    AND CFS.parent_type = 'FileGroup' AND CFS.parent_id = FG.id
+    ORDER BY FG.id ASC
+SQL
+    file_groups = connection.select_all(file_group_info_query).to_hash.collect {|h| h.with_indifferent_access}
+    Hash.new.tap do |hash|
+      file_groups.each do |file_group|
+        id = file_group[:id].to_i
+        hash[id] = file_group
+        if backup_info = backup_info_hash[id]
+          file_group[:backup_date] = backup_info[:date]
+          file_group[:backup_completed] = backup_info[:completed] ? 'Yes' : 'No'
+        else
+          file_group[:backup_date] = 'None'
+          file_group[:backup_completed] = 'N/A'
+        end
+      end
+    end
+  end
+
+  #hash from file_group_id to hash with latest backup date and whether it is complete, as judged from the part_count
+  #and archive_ids
+  def self.file_group_latest_amazon_backup_hash
+    query = <<SQL
+    SELECT FG.id AS file_group_id, AB.part_count, AB.archive_ids, AB.date FROM amazon_backups AB,
+    (SELECT cfs_directory_id, MAX(date) AS max_date FROM amazon_backups GROUP BY cfs_directory_id) ABLU,
+    cfs_directories CFS, file_groups FG
+    WHERE AB.cfs_directory_id = ABLU.cfs_directory_id AND AB.date = ABLU.max_date AND AB.part_count IS NOT NULL
+    AND AB.archive_ids IS NOT NULL AND CFS.id = AB.cfs_directory_id
+    AND FG.id = CFS.parent_id AND CFS.parent_type = 'FileGroup'
+SQL
+    backups = connection.select_rows(query)
+    Hash.new.tap do |hash|
+      backups.each do |file_group_id, part_count, archive_ids, date|
+        hash[file_group_id.to_i] = HashWithIndifferentAccess.new.tap do |backup_hash|
+          backup_hash[:date] = date
+          archives = YAML.load(archive_ids)
+          backup_hash[:completed] = (archives.present? && (archives.size == part_count.to_i) && archives.none? {|id| id.blank?})
+        end
+      end
+    end
+  end
+
+
 end
