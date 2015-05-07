55ea126f17a7497a1298e7fe2d8375619bcdf008
Howard Ding <hding2@illinois.edu>
2015-05-07 15:10:31 -0500

    Touch associated parents as appropriate

--- app/models/workflow/accrual_directory.rb
+++ app/models/workflow/accrual_directory.rb
@@ -1,5 +1,5 @@
 class Workflow::AccrualDirectory < ActiveRecord::Base
-  belongs_to :workflow_accrual_job
+  belongs_to :workflow_accrual_job, touch: true
 
   validates_presence_of :workflow_accrual_job_id
   validates_uniqueness_of :name, scope: :workflow_accrual_job_id, allow_blank: false
--- app/models/workflow/accrual_job.rb
+++ app/models/workflow/accrual_job.rb
@@ -1,6 +1,6 @@
 class Workflow::AccrualJob < ActiveRecord::Base
-  belongs_to :cfs_directory
-  belongs_to :user
+  belongs_to :cfs_directory, touch: true
+  belongs_to :user, touch: true
 
   has_many :workflow_accrual_directories, :class_name => 'Workflow::AccrualDirectory'
   has_many :workflow_accrual_files, :class_name => 'Workflow::AccrualFile'
