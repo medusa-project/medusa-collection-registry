78c3b38d9d3b5c212e7fdaf599980e334041fd03
Howard Ding <hding2@illinois.edu>
2015-05-29 16:57:33 -0500

    Update status via JS after doing an action. Always make 'View Report' available (to be changed again soon - make sure 'check' has been passed). Fix message when starting accrual.

--- app/controllers/accruals_controller.rb
+++ app/controllers/accruals_controller.rb
@@ -20,7 +20,7 @@ class AccrualsController < ApplicationController
     accrual_directories = params[:accrual][:accrual_directories].select {|d| d.present?}
     accrual_files = params[:accrual][:accrual_files].select {|f| f.present?}
     Workflow::AccrualJob.create_for(current_user, cfs_directory, staging_path, accrual_files, accrual_directories)
-    flash[:notice] = "Accrual to #{cfs_directory.relative_path} accepted. #{accrual_directories.count} #{'directory'.pluralize(accrual_directories.count)} and #{accrual_files.count} #{'file'.pluralize(accrual_files.count)} to be ingested."
+    flash[:notice] = "Your ingest request has been submitted to a Medusa Administrator and is pending approval."
     redirect_to cfs_directory
   end
 
