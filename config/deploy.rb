7509943950fdd9ff215eb4729844b9a671f946c2
Howard Ding <hding2@illinois.edu>
2015-06-04 16:03:40 -0500

    Update to latest cap version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.3.5'
+lock '3.4.0'
 
 set :application, 'medusa-collection-registry'
 set :repo_url, 'https://github.com/medusa-project/medusa-collection-registry.git'
