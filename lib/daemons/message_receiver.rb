03139f09b672d1ae0153298894500d2c483540e1
Howard Ding <hding2@illinois.edu>
2015-06-16 12:11:48 -0500

    Reinitialize AmqpConnector each run through the loop, attempting to fix some odd errors I've been getting.

--- lib/daemons/message_receiver.rb
+++ lib/daemons/message_receiver.rb
@@ -18,6 +18,7 @@ end
 #various types and perhaps from various queues. Maybe just run each type of message in its own thread so as not to
 #have to spawn a lot of these daemons?
 while ($running) do
+  AmqpConnector.instance.initialize
   AmqpResponse::AmazonBackup.handle_responses
   AmqpResponse::Fixity.handle_responses
   sleep 60
