55ff847e70b66908614959a7956e1d14747a1743
Howard Ding <hding2@illinois.edu>
2015-08-21 13:02:59 -0500

    Ensure cfs directory is present before running assessment.

--- app/controllers/bit_level_file_groups_controller.rb
+++ app/controllers/bit_level_file_groups_controller.rb
@@ -20,6 +20,7 @@ class BitLevelFileGroupsController < FileGroupsController
 
   def create_initial_cfs_assessment
     @file_group = BitLevelFileGroup.find(params[:id])
+    @file_group.ensure_cfs_directory
     authorize! :create_cfs_fits, @file_group
     if @file_group.is_currently_assessable?
       @file_group.schedule_initial_cfs_assessment
