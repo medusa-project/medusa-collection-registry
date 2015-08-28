fc6e5cc17d994965f235de0513ff9ce92d725269
Howard Ding <hding2@illinois.edu>
2015-08-28 11:47:01 -0500

    whitespace

--- app/models/cfs_directory_disk_comparison.rb
+++ app/models/cfs_directory_disk_comparison.rb
@@ -21,7 +21,7 @@ class CfsDirectoryDiskComparison < Object
   def disk_directory_missing?
     self.disk_directory_missing
   end
-  
+
   def print_disk_directory_missing
     puts "#{self.cfs_directory.id}:#{self.cfs_directory.absolute_path}"
     puts "MISSING ON DISK"
