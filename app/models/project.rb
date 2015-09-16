50a795b22fa5787b236aa79c0667b0fad56f46a7
Howard Ding <hding2@illinois.edu>
2015-09-16 13:39:48 -0500

    Fix up some things with respect to required fields for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,7 +7,7 @@ class Project < ActiveRecord::Base
 
   STATUSES = %w(active inactive completed)
 
-  validates_presence_of :start_date, :title, :manager, :owner
+  validates_presence_of :start_date, :title, :manager_id, :owner_id, :status
   validates_inclusion_of :status, in: STATUSES
 
   standard_auto_html(:specifications, :summary)
