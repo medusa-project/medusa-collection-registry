1e6389f5e9443a3c63793aba52be61b2118c29db
Howard Ding <hding2@illinois.edu>
2015-10-07 14:31:08 -0500

    Do additional caching of repo dashboard views Do some expensive operations only after we know that the cache is not good for repo dashboard views.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -31,15 +31,6 @@ class RepositoriesController < ApplicationController
   end
 
   def show_file_stats
-    fe_thread = Thread.new { @file_extension_hashes = ActiveRecord::Base.connection.
-        select_all('SELECT file_extension_id, extension, file_size, file_count FROM view_file_extension_stats_by_repository WHERE repository_id = $1', nil, [[nil, @repository.id]]).to_hash }
-    ct_thread = Thread.new { @content_type_hashes = ActiveRecord::Base.connection.
-        select_all('SELECT content_type_id, name, file_size, file_count FROM view_file_content_type_stats_by_repository WHERE repository_id = $1', nil, [[nil, @repository.id]]) }
-    blss_thread = Thread.new { @bit_level_storage_summary = ActiveRecord::Base.connection.
-        select_all('SELECT COALESCE(SUM(COALESCE(F.size,0)), 0) AS size, COUNT(*) AS count FROM view_cfs_files_to_parents V JOIN cfs_files F ON V.cfs_file_id = F.id WHERE V.repository_id = $1', nil, [[nil, @repository.id]]).to_hash.first }
-    fe_thread.join
-    ct_thread.join
-    blss_thread.join
     render partial: 'file_stats_table', layout: false
   end
 
@@ -53,7 +44,6 @@ class RepositoriesController < ApplicationController
   end
 
   def show_events
-    @events = @repository.cascaded_events.where('events.updated_at > ?', Time.now - 7.days).where(cascadable: true).includes(eventable: :parent)
     render partial: 'events', layout: false
   end
 
--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -10,4 +10,20 @@ module RepositoriesHelper
     tabs
   end
 
+  def load_repository_dashboard_file_stats
+    fe_thread = Thread.new { @file_extension_hashes = ActiveRecord::Base.connection.
+        select_all('SELECT file_extension_id, extension, file_size, file_count FROM view_file_extension_stats_by_repository WHERE repository_id = $1', nil, [[nil, @repository.id]]).to_hash }
+    ct_thread = Thread.new { @content_type_hashes = ActiveRecord::Base.connection.
+        select_all('SELECT content_type_id, name, file_size, file_count FROM view_file_content_type_stats_by_repository WHERE repository_id = $1', nil, [[nil, @repository.id]]) }
+    blss_thread = Thread.new { @bit_level_storage_summary = ActiveRecord::Base.connection.
+        select_all('SELECT COALESCE(SUM(COALESCE(F.size,0)), 0) AS size, COUNT(*) AS count FROM view_cfs_files_to_parents V JOIN cfs_files F ON V.cfs_file_id = F.id WHERE V.repository_id = $1', nil, [[nil, @repository.id]]).to_hash.first }
+    fe_thread.join
+    ct_thread.join
+    blss_thread.join
+  end
+
+  def load_repository_dashboard_events
+    @events = @repository.cascaded_events.where('events.updated_at > ?', Time.now - 7.days).where(cascadable: true).includes(eventable: :parent)
+  end
+
 end
