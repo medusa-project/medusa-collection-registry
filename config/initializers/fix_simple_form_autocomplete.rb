c9d46e33a92df34b0233c08feedf3ad3a56c93e8
Howard Ding <hding2@illinois.edu>
2015-10-19 11:59:13 -0500

    Remove workaround - fix has made it into gem

--- config/initializers/fix_simple_form_autocomplete.rb
+++ config/initializers/fix_simple_form_autocomplete.rb
@@ -1,14 +1,13 @@
 #Until rails3-jquery-autocomplete catches up this fixes a deprecation issue
 #with their simple_from integration. There is a pull request for this fix,
 #so hopefully it'll make it in soon after 1.0.14.
-
-require 'rails3-jquery-autocomplete'
-class SimpleForm::Inputs::AutocompleteInput
-  def input(wrapper_options)
-    @builder.autocomplete_field(
-        attribute_name,
-        options[:url],
-        merge_wrapper_options(rewrite_autocomplete_option, wrapper_options)
-    )
-  end
-end
+# require 'rails-jquery-autocomplete'
+# class SimpleForm::Inputs::AutocompleteInput
+#   def input(wrapper_options)
+#     @builder.autocomplete_field(
+#         attribute_name,
+#         options[:url],
+#         merge_wrapper_options(rewrite_autocomplete_option, wrapper_options)
+#     )
+#   end
+# end
