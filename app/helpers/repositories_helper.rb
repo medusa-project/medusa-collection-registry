90c2ef69fe0917abb529f012adf6641db5eacfa0
Howard Ding <hding2@illinois.edu>
2015-11-19 13:55:06 -0600

    Add tested info to repository dashboards.

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -10,14 +10,36 @@ module RepositoriesHelper
 
   def load_repository_dashboard_file_stats
     fe_thread = Thread.new { @file_extension_hashes = ActiveRecord::Base.connection.
-        select_all('SELECT file_extension_id, extension, file_size, file_count FROM view_file_extension_stats_by_repository WHERE repository_id = $1', nil, [[nil, @repository.id]]).to_hash }
+        select_all(load_repository_dashboard_file_extension_sql, nil, [[nil, @repository.id]]).to_hash }
     ct_thread = Thread.new { @content_type_hashes = ActiveRecord::Base.connection.
-        select_all('SELECT content_type_id, name, file_size, file_count FROM view_file_content_type_stats_by_repository WHERE repository_id = $1', nil, [[nil, @repository.id]]) }
+        select_all(load_repository_dashboard_content_type_sql, nil, [[nil, @repository.id]]) }
     blss_thread = Thread.new { @bit_level_storage_summary = ActiveRecord::Base.connection.
         select_all('SELECT COALESCE(SUM(COALESCE(F.size,0)), 0) AS size, COUNT(*) AS count FROM view_cfs_files_to_parents V JOIN cfs_files F ON V.cfs_file_id = F.id WHERE V.repository_id = $1', nil, [[nil, @repository.id]]).to_hash.first }
     fe_thread.join
     ct_thread.join
     blss_thread.join
+  end
+
+  def load_repository_dashboard_content_type_sql
+    <<SQL
+    SELECT CTS.content_type_id, CTS.name, CTS.file_size, CTS.file_count,
+    COALESCE(CTC.count,0) AS tested_count
+    FROM view_file_content_type_stats_by_repository CTS
+    LEFT JOIN (SELECT content_type_id, count FROM view_tested_file_content_type_counts WHERE repository_id = $1) CTC
+    ON CTS.content_type_id = CTC.content_type_id
+    WHERE repository_id = $1
+SQL
+  end
+
+  def load_repository_dashboard_file_extension_sql
+    <<SQL
+    SELECT FES.file_extension_id, FES.extension, FES.file_size, FES.file_count,
+    COALESCE(FEC.count,0) AS tested_count
+    FROM view_file_extension_stats_by_repository FES
+    LEFT JOIN (SELECT file_extension_id, count FROM view_tested_file_file_extension_counts WHERE repository_id = $1) FEC
+    ON FES.file_extension_id = FEC.file_extension_id
+    WHERE repository_id = $1
+SQL
   end
 
   def load_repository_dashboard_events
