806730f87b46f08f26da0de70071ff945c33ddd7
Howard Ding <hding2@illinois.edu>
2015-11-20 12:25:41 -0600

    Remove constraint that barcode must exist, but set it to an empty string if it is nil.

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,15 +2,18 @@ class Item < ActiveRecord::Base
   belongs_to :project, touch: true
   delegate :title, to: :project, prefix: true
 
-  validates :barcode, presence: true
-
-  auto_strip_attributes :barcode
+  before_validation :ensure_barcode
+  auto_strip_attributes :barcode, nullify: false
 
   searchable include: :project do
     text :barcode
     string :barcode, stored: true
     string :title
     string :project_title
+  end
+
+  def ensure_barcode
+    self.barcode ||= ''
   end
 
 end
