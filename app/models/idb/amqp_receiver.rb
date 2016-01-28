1cb9f795c080d6c6e55d3500388cde83ef592937
Howard Ding <hding2@illinois.edu>
2016-01-28 10:24:22 -0600

    Fixing git/case-insensititve issue

---
+++ app/models/idb/amqp_receiver.rb
@@ -0,0 +1,27 @@
+module Idb
+  class AmqpReceiver < Object
+
+    def self.handle_responses
+      AmqpConnector.instance.with_queue(Config.instance.incoming_queue) do |queue|
+        while true
+          delivery_info, properties, raw_payload = queue.pop
+          Rails.logger.error("Handling IDB message #{raw_payload}")
+          break unless raw_payload
+          handle_message(JSON.parse(raw_payload))
+        end
+      end
+    end
+
+    def self.handle_message(message)
+      case message['operation']
+        when 'ingest'
+          Idb::IngestJob.create_for(message)
+        else
+          raise RuntimeError, "Unrecognized operation requested"
+      end
+    rescue Exception
+      Rails.logger.error "Failed to create IDB Ingest Job for message: #{message}"
+      raise
+    end
+  end
+end
