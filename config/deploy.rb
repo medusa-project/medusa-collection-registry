87676907b5dce138bd4a17d1bd5fb7b9279b9214
Howard Ding <hding2@illinois.edu>
2016-03-28 15:25:52 -0500

    link downloader.yml config file on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :rails_env, 'production'
 
 # Default value for :linked_files is []
 set :linked_files, %w(config/database.yml config/shibboleth.yml config/handle_client.yml config/fits_service.yml
-  config/medusa.yml config/smtp.yml config/sunspot.yml config/idb.yml)
+  config/medusa.yml config/smtp.yml config/sunspot.yml config/idb.yml config/downloader.yml)
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'tmp/item_upload_csv')
