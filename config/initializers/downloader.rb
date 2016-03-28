fa5d09c459a2be411d01f7b42f2915d5419ba25f
Howard Ding <hding2@illinois.edu>
2016-03-28 17:08:29 -0500

    Revert - need to do amqp this way

--- config/initializers/downloader.rb
+++ config/initializers/downloader.rb
@@ -1,14 +1,12 @@
 Application.downloader_config =
     Downloader::Config.new(YAML.load_file(File.join(Rails.root, 'config', 'downloader.yml'))[Rails.env])
 
-AmqpConnector.new(:downloader, Application.downloader_config.amqp(default: Hash.new).symbolize_keys)
-
-# if defined?(PhusionPassenger)
-#   PhusionPassenger.on_event(:starting_worker_process) do |forked|
-#     if forked
-#       AmqpConnector.new(:downloader, Application.downloader_config.amqp(default: Hash.new).symbolize_keys)
-#     end
-#   end
-# else
-#   AmqpConnector.new(:downloader, Application.downloader_config.amqp(default: Hash.new).symbolize_keys)
-# end
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    if forked
+      AmqpConnector.new(:downloader, Application.downloader_config.amqp(default: Hash.new).symbolize_keys)
+    end
+  end
+else
+  AmqpConnector.new(:downloader, Application.downloader_config.amqp(default: Hash.new).symbolize_keys)
+end
