e32290fe27e8d53e6e59d10a62f46e102a0930fe
Howard Ding <hding2@illinois.edu>
2016-03-28 16:25:43 -0500

    trying to diagnose amqp issue

--- config/initializers/downloader.rb
+++ config/initializers/downloader.rb
@@ -1,11 +1,10 @@
 Application.downloader_config =
     Downloader::Config.new(YAML.load_file(File.join(Rails.root, 'config', 'downloader.yml'))[Rails.env])
 
-AmqpConnector.new(:downloader, Application.downloader_config.amqp(default: Hash.new).symbolize_keys)
-
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
     if forked
+      AmqpConnector.new(:downloader, Application.downloader_config.amqp(default: Hash.new).symbolize_keys)
       begin
         config = Application.downloader_config
         connection = Bunny.new(config.amqp)
@@ -28,4 +27,6 @@ if defined?(PhusionPassenger)
       end
     end
   end
+else
+  AmqpConnector.new(:downloader, Application.downloader_config.amqp(default: Hash.new).symbolize_keys)
 end
