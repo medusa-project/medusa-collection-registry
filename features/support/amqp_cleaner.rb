3f63773b1f8e7692aa6577900b29187313138ed8
Howard Ding <hding2@illinois.edu>
2016-04-01 15:42:16 -0500

    clear amqp queues at the end of a test run

--- features/support/amqp_cleaner.rb
+++ features/support/amqp_cleaner.rb
@@ -5,3 +5,8 @@ Before do
   AmqpConnector.clear_all_queues
 end
 
+#make sure things are cleaned up at the end, too. Sometimes a break in the middle of
+#a test can leave things in an incorrect state for the next run
+at_exit do
+  AmqpConnector.clear_all_queues
+end
