40c403855d63f76b41d75aee59f3e5cb910d89cb
Howard Ding <hding2@illinois.edu>
2016-06-10 12:40:12 -0500

    Update idb tests to use anticipated more general amqp_accrual structure (itself not yet implemented)

--- features/step_definitions/idb/ingest_idb_file.rb
+++ features/step_definitions/idb/ingest_idb_file.rb
@@ -1,42 +1,42 @@
 #steps for idb file ingest
 
 When(/^IDB sends an ingest request$/) do
-  AmqpConnector.connector(:medusa).send_message(Idb::Config.instance.incoming_queue, IdbTestHelper.idb_ingest_message)
+  AmqpConnector.connector(:medusa).send_message(AmqpAccrual::Config.incoming_queue('idb'), IdbTestHelper.idb_ingest_message)
 end
 
 And(/^Medusa picks up the IDB ingest request$/) do
-  Idb::AmqpReceiver.handle_responses
+  AmqpAccrual::Receiver.handle_response('idb')
 end
 
 Then(/^there should be an IDB ingest delayed job reflecting the ingest request$/) do
   message = IdbTestHelper.idb_ingest_message
-  expect(Idb::IngestJob.find_by(staging_path: message['staging_path'])).to be_truthy
+  expect(AmqpAccrual::IngestJob.find_by(staging_path: message['staging_path'], client: 'idb')).to be_truthy
 end
 
 Given(/^there is an IDB ingest delayed job$/) do
   IdbTestHelper.stage_content
-  Idb::IngestJob.create_for(IdbTestHelper.idb_ingest_message)
+  AmqpAccrual::IngestJob.create_for('idb', IdbTestHelper.idb_ingest_message)
 end
 
 And(/^there is an IDB file group$/) do
   file_group = FactoryGirl.create(:bit_level_file_group)
-  Idb::Config.instance.idb_file_group_id = file_group.id
+  AmqpAccrual::Config.set_file_group_id('idb', file_group.id)
 end
 
 When(/^the IDB ingest delayed job is run$/) do
-  Idb::IngestJob.first.perform
+  AmqpAccrual::IngestJob.where(client: 'idb').first.perform
 end
 
 Then(/^the IDB files should be present in medusa storage$/) do
-  expect(Idb::Config.instance.idb_file_group.total_files).to eq(1)
-  Idb::Config.instance.idb_cfs_directory.each_file_in_tree do |file|
+  expect(AmqpAccrual::Config.file_group('idb').total_files).to eq(1)
+  AmqpAccrual::Config.cfs_directory('idb').each_file_in_tree do |file|
     expect(file.name).to eq('file.txt')
     expect(File.read(file.absolute_path)).to match('Staging text')
   end
 end
 
 And(/^Medusa should have sent a return message to IDB$/) do
-  AmqpConnector.connector(:medusa).with_message(Idb::Config.instance.outgoing_queue) do |raw_message|
+  AmqpConnector.connector(:medusa).with_message(AmqpAccrual::Config.outgoing_queue('idb')) do |raw_message|
     message = JSON.parse(raw_message)
     expect(message['operation']).to eq('ingest')
     expect(message['status']).to eq('ok')
