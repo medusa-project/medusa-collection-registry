e300a5723e2220540dd3e6c91092dd4eea87c5cd
Howard Ding <hding2@illinois.edu>
2016-06-30 13:06:50 -0500

    Finish indicator icon for searches.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,3 +1,6 @@
-function indicate_search_results_in_tab_header(record_count, table_id) {
-    
+function indicate_search_results_in_tab_header(record_count, tab_id) {
+    var icon = $('i', $("a[href='" + tab_id + "']"));
+    var new_icon = (record_count > 0) ? 'fa-check' : 'fa-times';
+    var new_color = (record_count > 0) ? 'search-hit' : 'search-missed';
+    icon.attr('class', _.string.join(' ', 'fa', new_icon, new_color));
 }
