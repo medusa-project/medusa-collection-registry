fd0115e8430da132e127e18385e8ef1d0028f51b
Howard Ding <hding2@illinois.edu>
2016-08-23 09:56:25 -0500

    Fold events.yml into settings.yml

--- app/models/concerns/eventable.rb
+++ app/models/concerns/eventable.rb
@@ -7,20 +7,17 @@ module Eventable
     has_many :events, -> { order 'date DESC' }, as: :eventable, dependent: :destroy
     class_attribute :supported_event_hash
     initialize_event_hash(self.to_s.underscore)
+    delegate :supported_event_hash, to: :class
   end
 
   module ClassMethods
     def initialize_event_hash(config_key)
-      self.supported_event_hash = read_event_hash(config_key)
+      self.supported_event_hash = read_event_hash(config_key).to_h.stringify_keys
     end
 
     def read_event_hash(group_key)
-      YAML.load_file(File.join(Rails.root, 'config', 'events.yml'))[group_key.to_s]
+      Settings.events[group_key]
     end
-  end
-
-  def supported_event_hash
-    self.class.supported_event_hash
   end
 
   def supported_event_keys
