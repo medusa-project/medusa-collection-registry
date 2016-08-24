af87a0e9fa32697601c6ffd6c5ab09079bfc9d1f
Howard Ding <hding2@illinois.edu>
2016-08-24 10:28:36 -0500

    change default ordering of file format profiles table

--- app/assets/javascripts/file_format_profiles.js
+++ app/assets/javascripts/file_format_profiles.js
@@ -1,3 +1,5 @@
 $(document).ready(function () {
-  initialize_data_table("table#file_format_profiles", {})
+  initialize_data_table("table#file_format_profiles", {
+      "order" : [[1, "asc"], [0 , "asc"]]
+  })
 })
