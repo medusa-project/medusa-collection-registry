fe3c867ce801e57d2b536e166d77be08ce4a561b
Howard Ding <hding2@illinois.edu>
2016-08-31 12:41:39 -0500

    Add comment about adding new previewers

--- app/models/preview/base.rb
+++ app/models/preview/base.rb
@@ -1,3 +1,8 @@
+#To add a new previewer for a CfsFile (via the show action), you basically need to
+#- add the appropriate content types/extensions to config/settings.yml with a new key
+#- make Preview::Resolver resolve that key to a new subclass of Preview::Base
+#- implement view_partial on that subclass
+#- add that view partial to the cfs file views and implement any new controller actions that are needed by the view
 module Preview
   class Base < Object
     attr_accessor :cfs_file
