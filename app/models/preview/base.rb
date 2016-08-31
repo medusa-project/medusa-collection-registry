796560835ea750e0c9ad6b63296b6813cafce5f3
Howard Ding <hding2@illinois.edu>
2016-08-31 13:16:36 -0500

    Add action to send thumbnail for images

--- app/models/preview/base.rb
+++ app/models/preview/base.rb
@@ -2,7 +2,8 @@
 #- add the appropriate content types/extensions to config/settings.yml with a new key
 #- make Preview::Resolver resolve that key to a new subclass of Preview::Base
 #- implement view_partial on that subclass
-#- add that view partial to the cfs file views and implement any new controller actions that are needed by the view
+#- add that view partial to the cfs file views and implement any new controller actions/routes that are needed by the view
+#Note that you may also want to implement functionality here that doesn't really belong in the controller, e.g. for Preview::Image
 module Preview
   class Base < Object
     attr_accessor :cfs_file
