a95a389924588656ed0fb562b52315e4203e3295
Howard Ding <hding2@illinois.edu>
2016-08-31 17:24:08 -0500

    Only display viewer if there are some images to view

--- app/helpers/cfs_files_helper.rb
+++ app/helpers/cfs_files_helper.rb
@@ -3,7 +3,8 @@ module CfsFilesHelper
   def galleria_data_hashes(directory)
     image_files = directory.cfs_files.includes(:content_type, :file_extension).select { |f| f.preview_type_is_image? }
     image_files.collect do |f|
-      {image: galleria_cfs_file_path(f), thumb: thumbnail_cfs_file_path(f), link: cfs_file_path(f)}
+      {image: galleria_cfs_file_path(f), thumb: thumbnail_cfs_file_path(f), link: cfs_file_path(f),
+       title: f.name}
     end
   end
 
