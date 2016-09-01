5eddbf4ea09052bc5a23f775d67537f8e737a20a
Howard Ding <hding2@illinois.edu>
2016-09-01 12:36:58 -0500

    fix typo

--- app/helpers/cfs_files_helper.rb
+++ app/helpers/cfs_files_helper.rb
@@ -1,7 +1,7 @@
 module CfsFilesHelper
 
   def galleria_data_hashes(directory)
-    image_files = directory.cfs_files.sort('cfs_files.name asc').includes(:content_type, :file_extension).select { |f| f.preview_type_is_image? }
+    image_files = directory.cfs_files.order('cfs_files.name asc').includes(:content_type, :file_extension).select { |f| f.preview_type_is_image? }
     image_files.collect do |f|
       {image: galleria_cfs_file_path(f), thumb: thumbnail_cfs_file_path(f), link: cfs_file_path(f),
        title: f.name}
