f7cdbdac3fcf0c46913cc3f1fe1036938939e995
Howard Ding <hding2@illinois.edu>
2017-03-21 15:36:54 -0500

    fix typo

--- app/models/search_helper/base.rb
+++ app/models/search_helper/base.rb
@@ -77,7 +77,11 @@ class SearchHelper::Base < Object
   #this provides a general way to select the indexes of a set of columns - just make the symbol present in the column spec hash
   #for datatables often it is useful to have such an array of indexes
   def columns_tagged(symbol)
-    columns.each.with_index.collect { |spec, index| [spec, index] }.select { |pair| pair.first[:symbol].present? }.collect { |pair| pair.second }
+    columns.each.with_index.collect { |spec, index| [spec, index] }.select { |pair| pair.first[symbol].present? }.collect { |pair| pair.second }
+  end
+
+  def columns_not_tagged(symbol)
+    columns.each.with_index.collect { |spec, index| [spec, index] }.reject { |pair| pair.first[symbol].present? }.collect { |pair| pair.second }
   end
 
   def search
