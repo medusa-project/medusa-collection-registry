6819ab0b9c090ca7c54f5ec9cd7922ea0e614877
Howard Ding <hding2@illinois.edu>
2017-06-05 13:27:50 -0500

    Don't link bin directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :rails_env, 'production'
 set :linked_files, %w(config/database.yml config/sunspot.yml config/settings/production.local.yml)
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle',
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle',
                                                'public/system', 'tmp/item_upload_csv')
 
 # Default value for default_env is {}
