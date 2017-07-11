5237c665539ee355165d8c78295e1b0196f10981
Howard Ding <hding2@illinois.edu>
2017-07-11 12:41:51 -0500

    This got changed incorrectly in an auto-refactor.

--- db/migrate/20170710202254_create_job_report_producers.rb
+++ db/migrate/20170710202254_create_job_report_producers.rb
@@ -2,7 +2,7 @@ class CreateJobReportProducers < ActiveRecord::Migration[5.1]
   def change
     create_table :job_report_producers do |t|
       t.references :user, foreign_key: true
-      t.references :producer_mailer, foreign_key: true
+      t.references :producer, foreign_key: true
 
       t.timestamps
     end
