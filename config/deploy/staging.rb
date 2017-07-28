672040d236ddabbd5aed9d2d6c986fdaa781d6e7
Howard Ding <hding2@illinois.edu>
2017-07-28 15:56:31 -0500

    Update for new setup

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -15,6 +15,7 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
+set :home, '/home/lib-medusa-collectionregistry'
 server 'medusatest.library.illinois.edu', user: 'lib-medusa-collectionregistry', roles: %w{web app db}, primary: true
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
