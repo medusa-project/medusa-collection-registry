a3933837c936a651d4128e369b8c2d25bbdfd2b2
Howard Ding <hding2@illinois.edu>
2017-11-07 11:18:23 -0600

    Update datatables, including more plugins as well Add CSV button to export file formats

--- app/assets/javascripts/data_tables_common.js
+++ app/assets/javascripts/data_tables_common.js
@@ -1,47 +1,51 @@
 var default_datatable_args = {
-  "order": [
-    [0, "asc"]
-  ],
-  "lengthMenu": [
-    [10, 25, 50, 100, -1],
-    [10, 25, 50, 100, "All"]
-  ],
-  "pageLength": 25,
-  "stateSave": "true",
-  "stateSaveCallback": function (settings, data) {
-    localStorage.setItem('DataTables_' + window.location.pathname, JSON.stringify(data));
-  },
-  "stateLoadCallback": function (settings) {
-    return JSON.parse(localStorage.getItem('DataTables_' + window.location.pathname));
-  },
-  "language": {
-    "search": "Filter results: ",
-    "paginate": {
-      "next": "<i class='fa fa-chevron-right' title='next'><\i>",
-      "previous": "<i class='fa fa-chevron-left' title='previous'><\i>"
-    }
-  },
-  "processing": true,
-  "pagingType": 'full_numbers'
+    "order": [
+        [0, "asc"]
+    ],
+    "lengthMenu": [
+        [10, 25, 50, 100, -1],
+        [10, 25, 50, 100, "All"]
+    ],
+    "pageLength": 25,
+    "stateSave": "true",
+    "stateSaveCallback": function (settings, data) {
+        localStorage.setItem('DataTables_' + window.location.pathname, JSON.stringify(data));
+    },
+    "stateLoadCallback": function (settings) {
+        return JSON.parse(localStorage.getItem('DataTables_' + window.location.pathname));
+    },
+    "language": {
+        "search": "Filter results: ",
+        "paginate": {
+            "next": "<i class='fa fa-chevron-right' title='next'><\i>",
+            "previous": "<i class='fa fa-chevron-left' title='previous'><\i>"
+        }
+    },
+    "processing": true,
+    "pagingType": 'full_numbers'
 };
 
 //Make the table selected by the provided CSS selector into a dataTables table using the default_datatable_args with
 //the extra_args deep merged in.
 function initialize_data_table(tableSelector, extra_args) {
-  var args = $.extend(true, {}, default_datatable_args, extra_args);
-  $(function () {
+    $(function () {
+        initialize_data_table_synchronous(tableSelector, extra_args)
+    })
+};
+
+function initialize_data_table_synchronous(tableSelector, extra_args) {
+    var args = $.extend(true, {}, default_datatable_args, extra_args);
     try {
-      $(tableSelector).DataTable(args);
-      add_back_to_top_button(tableSelector);
-    } catch(err) {
+        $(tableSelector).DataTable(args);
+        add_back_to_top_button(tableSelector);
+    } catch (err) {
 
     }
-  })
-};
+}
 
 //Add a back to top of page button to the datatable
 function add_back_to_top_button(tableSelector) {
-  var pagination_list = $(tableSelector).closest('div.row').next('div').find('ul.pagination');
-  var up_button = '<li class="paginate_button"><a href="#global-navigation"><i class="fa fa-chevron-circle-up"></i></a></li>';
-  pagination_list.append(up_button);
+    var pagination_list = $(tableSelector).closest('div.row').next('div').find('ul.pagination');
+    var up_button = '<li class="paginate_button"><a href="#global-navigation"><i class="fa fa-chevron-circle-up"></i></a></li>';
+    pagination_list.append(up_button);
 }
--- app/assets/javascripts/file_formats.js
+++ app/assets/javascripts/file_formats.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
-  initialize_data_table("table#file_formats", {})
+  initialize_data_table_synchronous("table#file_formats", {
+      'dom': "<'row'<'col-sm-2'l><'col-sm-4'B><'col-sm-6'f>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+      'buttons': ['csv'],
+  });
 })
