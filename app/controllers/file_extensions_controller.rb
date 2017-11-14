104815defe30621c8dfe8cd807c6070bffe3d943
Howard Ding <hding2@illinois.edu>
2017-11-14 11:36:52 -0600

    Fix typo.

--- app/controllers/file_extensions_controller.rb
+++ app/controllers/file_extensions_controller.rb
@@ -54,8 +54,8 @@ class FileExtensionsController < ApplicationController
     @file_extension = FileExtension.find(params[:id])
   end
 
-  def create_download_package(cfs_files, current_user)
-    Downloader::Request.create_for_file_list(cfs_files)
+  def create_download_package(cfs_files)
+    Downloader::Request.create_for_file_list(cfs_files, current_user)
   end
 
 end
