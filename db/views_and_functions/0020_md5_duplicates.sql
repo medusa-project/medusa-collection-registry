af7419e9c7cfccebe35818893e788a291d180ec5
Howard Ding <hding2@illinois.edu>
2017-11-17 16:59:46 -0600

    Add view for md5 duplicates. Efficiency is still a concern.

---
+++ db/views_and_functions/0020_md5_duplicates.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE VIEW md5_duplicates
+  AS
+    SELECT
+      F.md5_sum,
+      V.cfs_file_id,
+      V.cfs_directory_id,
+      V.file_group_id,
+      V.collection_id,
+      V.repository_id,
+      U.uuid,
+      F.name,
+      cfs_file_relative_path(F.id) AS relative_path,
+      C.name                       AS content_type,
+      F.mtime
+    FROM view_cfs_files_to_parents V,
+      cfs_files F,
+      medusa_uuids U,
+      content_types C
+    WHERE F.id = V.cfs_file_id
+          AND U.uuidable_type = 'CfsFile'
+          AND U.uuidable_id = F.id
+          AND F.content_type_id = C.id
+          AND F.md5_sum IN
+              (SELECT md5_sum
+               FROM
+                 (SELECT
+                    md5_sum,
+                    count(*)
+                  FROM cfs_files
+                  GROUP BY md5_sum
+                  HAVING count(*) > 1) AS duplicates);
