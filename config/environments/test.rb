2bb273db2ed576d8f90af4c4147ae87a8c6bb3f1
Howard Ding <hding2@illinois.edu>
2018-02-07 15:11:15 -0600

    Having a symbol here was causing a puma error, which seems to have been preventing loading the webpack javascript correctly.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,7 +9,7 @@ MedusaCollectionRegistry::Application.configure do
 
   # Configure static asset server for tests with Cache-Control for performance
   config.public_file_server.enabled = true
-  config.public_file_server.headers = {'Cache-Control': 'public, max-age=3600'}
+  config.public_file_server.headers = {'Cache-Control' => 'public, max-age=3600'}
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
