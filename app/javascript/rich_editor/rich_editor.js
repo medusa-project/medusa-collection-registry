c5658f83379a777cfffb23ff0e15b6b8fa0dd38b
Howard Ding <hding2@illinois.edu>
2018-02-15 17:45:08 -0600

    Add plugins/etc. to make the tinymce functionality similar to what we had with CKEditor. Remove some things that don't seem relevant, though.

--- app/javascript/rich_editor/rich_editor.js
+++ app/javascript/rich_editor/rich_editor.js
@@ -1,11 +1,36 @@
 require('tinymce/themes/modern');
 import tinymce from 'tinymce';
+
 require('tinymce/skins/lightgray/content.min.css');
 require('tinymce/skins/lightgray/skin.min.css');
 
-tinymce.init({
-      selector: 'textarea.rich-editor',
-      //necessary skin files are included above
-      skin: false
-    }
-)
+require('tinymce/plugins/code');
+require('tinymce/plugins/link');
+require('tinymce/plugins/anchor');
+require('tinymce/plugins/paste');
+require('tinymce/plugins/searchreplace');
+require('tinymce/plugins/lists');
+require('tinymce/plugins/textcolor');
+require('tinymce/plugins/colorpicker');
+require('tinymce/plugins/fullscreen');
+require('tinymce/plugins/charmap');
+require('tinymce/plugins/hr');
+
+require('./rich_editor.scss');
+
+$(document).ready(() => {
+  tinymce.init({
+        selector: 'textarea.rich-editor',
+        //necessary skin files are included above
+        skin: false,
+        plugins: 'code link anchor paste searchreplace lists textcolor colorpicker fullscreen charmap hr',
+        toolbar: [
+          'cut copy paste pastetext | undo redo | searchreplace | bold italic underline strikethrough subscript superscript | removeformat | hr charmap',
+          'numlist bullist | outdent indent | blockquote | alignleft aligncenter alignright alignjustify | link unlink anchor  | code fullscreen',
+          'styleselect formatselect fontselect fontsizeselect | forecolor backcolor'
+        ],
+        fullscreen_new_window: true
+      }
+  )
+})
+
