b9d6afd8443a1a4c37d7dff0092bd3863e261c1d
Howard Ding <hding2@illinois.edu>
2018-02-15 18:08:32 -0600

    Add code to hide most of the tinymce stuff until the textarea is selected.

--- app/javascript/rich_editor/rich_editor.js
+++ app/javascript/rich_editor/rich_editor.js
@@ -20,17 +20,33 @@ require('./rich_editor.scss');
 
 $(document).ready(() => {
   tinymce.init({
-        selector: 'textarea.rich-editor',
-        //necessary skin files are included above
-        skin: false,
-        plugins: 'code link anchor paste searchreplace lists textcolor colorpicker fullscreen charmap hr',
-        toolbar: [
-          'cut copy paste pastetext | undo redo | searchreplace | bold italic underline strikethrough subscript superscript | removeformat | hr charmap',
-          'numlist bullist | outdent indent | blockquote | alignleft aligncenter alignright alignjustify | link unlink anchor  | code fullscreen',
-          'styleselect formatselect fontselect fontsizeselect | forecolor backcolor'
-        ],
-        branding: false,
-      }
+    selector: 'textarea.rich-editor',
+    //necessary skin files are included above
+    skin: false,
+    plugins: 'code link anchor paste searchreplace lists textcolor colorpicker fullscreen charmap hr',
+    toolbar: [
+      'cut copy paste pastetext | undo redo | searchreplace | bold italic underline strikethrough subscript superscript | removeformat | hr charmap',
+      'numlist bullist | outdent indent | blockquote | alignleft aligncenter alignright alignjustify | link unlink anchor  | code fullscreen',
+      'styleselect formatselect fontselect fontsizeselect | forecolor backcolor'
+    ],
+    branding: false,
+    setup: function (theEditor) {
+      theEditor.on('focus', function () {
+        $(this.contentAreaContainer.parentElement).find("div.mce-toolbar-grp").show();
+        $(this.contentAreaContainer.parentElement).find("div.mce-menubar").show();
+        $(this.contentAreaContainer.parentElement).find("div.mce-statusbar").show();
+      });
+      theEditor.on('blur', function () {
+        $(this.contentAreaContainer.parentElement).find("div.mce-toolbar-grp").hide();
+        $(this.contentAreaContainer.parentElement).find("div.mce-menubar").hide();
+        $(this.contentAreaContainer.parentElement).find("div.mce-statusbar").hide();
+      });
+      theEditor.on("init", function () {
+        $(this.contentAreaContainer.parentElement).find("div.mce-toolbar-grp").hide();
+        $(this.contentAreaContainer.parentElement).find("div.mce-menubar").hide();
+        $(this.contentAreaContainer.parentElement).find("div.mce-statusbar").hide();
+      })}
+    }
   )
 })
 
