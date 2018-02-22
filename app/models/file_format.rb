070bacf2a6d0a9c4895eebc292ed1e9c2f24352a
Howard Ding <hding2@illinois.edu>
2018-02-22 16:03:56 -0600

    Add logic for handling logical extension strings

--- app/models/file_format.rb
+++ app/models/file_format.rb
@@ -7,4 +7,17 @@ class FileFormat < ApplicationRecord
 
   has_many :file_formats_logical_extensions_joins, dependent: :destroy
   has_many :logical_extensions, -> {order 'extension asc, description asc'}, through: :file_formats_logical_extensions_joins
+
+  def logical_extensions_string
+    logical_extensions.collect {|extension| extension.label}.join(', ')
+  end
+
+  def logical_extensions_string=(extensions)
+    if extensions.strip.blank?
+      self.logical_extensions = []
+    else
+      self.logical_extensions = extensions.split(',').collect {|ext| LogicalExtension.ensure_extension(ext)}
+    end
+  end
+  
 end
