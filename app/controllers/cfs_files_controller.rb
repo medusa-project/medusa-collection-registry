0c451e0e05729405efdfcadb6e2d46be94b7e3d7
Howard Ding <hding2@illinois.edu>
2018-05-04 17:37:06 -0500

    Make just one method to serve content to previewers that need it.

--- app/controllers/cfs_files_controller.rb
+++ app/controllers/cfs_files_controller.rb
@@ -5,10 +5,10 @@ class CfsFilesController < ApplicationController
   before_action :require_medusa_user, except: [:show, :download]
   before_action :require_medusa_user_or_basic_auth, only: [:show, :download]
   before_action :find_file, only: [:show, :create_fits_xml, :fits, :download, :view, :fixity_check, :events,
-                                   :preview_image, :preview_video, :preview_audio, :preview_pdf,
-                                   :preview_pdf_content, :preview_iiif_image,
+                                   :preview_image, :preview_content, :preview_pdf,
+                                   :preview_iiif_image,
                                    :thumbnail, :galleria]
-  before_action :find_previewer, only: [:show, :preview_image, :preview_video, :preview_audio, :preview_pdf, :preview_iiif_image, :thumbnail, :galleria]
+  before_action :find_previewer, only: [:show, :preview_image, :preview_content, :preview_pdf, :preview_iiif_image, :thumbnail, :galleria]
 
   def show
     @file_group = @file.file_group
@@ -98,12 +98,7 @@ class CfsFilesController < ApplicationController
   end
 
 
-  def preview_video
-    authorize! :download, @file.file_group
-    send_file @file.absolute_path, type: safe_content_type(@file), disposition: 'inline', range: true, buffer_size: 100000
-  end
-
-  def preview_audio
+  def preview_content
     authorize! :download, @file.file_group
     send_file @file.absolute_path, type: safe_content_type(@file), disposition: 'inline', range: true, buffer_size: 100000
   end
@@ -111,11 +106,6 @@ class CfsFilesController < ApplicationController
   def preview_pdf
     authorize! :download, @file.file_group
     render layout: nil
-  end
-
-  def preview_pdf_content
-    authorize! :download, @file.file_group
-    send_file @file.absolute_path, type: safe_content_type(@file), disposition: 'inline', range: true, buffer_size: 100000
   end
 
   def random
