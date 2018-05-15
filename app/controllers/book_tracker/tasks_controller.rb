2dc9d72310abdd5282482161ff9edd1071c21396
Alex Dolski <alexd@n-ary.net>
2018-05-15 11:01:26 -0500

    Modify the Google Books check to work around the new Google authentication (MBT-56)

--- app/controllers/book_tracker/tasks_controller.rb
+++ app/controllers/book_tracker/tasks_controller.rb
@@ -2,20 +2,34 @@ module BookTracker
 
   class TasksController < ApplicationController
 
+    TEMP_DIR = Rails.root.join('public', 'system', 'book_tracker')
+
     before_action :require_book_tracker_admin
     before_action :service_check_in_progress, except: :index
 
     ##
-    # Responds to POST /check-google
+    # Responds to POST /book_tracker/check-google
     #
     def check_google
-      Delayed::Job.enqueue(GoogleJob.new)
-      flash['success'] = 'Google check will begin momentarily.'
+      uploaded_io = params[:file]
+      if uploaded_io.respond_to?(:original_filename)
+        FileUtils.makedirs(TEMP_DIR)
+        pathname = File.join(TEMP_DIR, "google_books_#{SecureRandom.uuid}.txt")
+
+        File.open(pathname, 'wb') do |file|
+          file.write(uploaded_io.read)
+        end
+
+        Delayed::Job.enqueue(GoogleJob.new(pathname))
+        flash['success'] = 'Google check will begin momentarily.'
+      else
+        flash['error'] = 'No file provided.'
+      end
       redirect_back fallback_location: book_tracker_tasks_path
     end
 
     ##
-    # Responds to POST /check-hathitrust
+    # Responds to POST /book_tracker/check-hathitrust
     #
     def check_hathitrust
       Delayed::Job.enqueue(HathitrustJob.new)
@@ -24,7 +38,7 @@ module BookTracker
     end
 
     ##
-    # Responds to POST /check-internet-archive
+    # Responds to POST /book_tracker/check-internet-archive
     #
     def check_internet_archive
       Delayed::Job.enqueue(InternetArchiveJob.new)
@@ -33,7 +47,7 @@ module BookTracker
     end
 
     ##
-    # Responds to POST /import
+    # Responds to POST /book_tracker/import
     #
     def import
       Delayed::Job.enqueue(ImportJob.new)
