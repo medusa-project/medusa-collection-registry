331e0a9dd76105387baefe8c44cad1d108dca163
Howard Ding <hding2@illinois.edu>
2018-07-03 09:38:53 -0500

    Remove CfsRoot reference

--- features/support/directory_cleaner.rb
+++ features/support/directory_cleaner.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 #before each test make sure that the cfs directories, any staging directories, item_upload_csv directories are empty
 Before do
 
-  [CfsDirectory.export_root, CfsRoot.instance.path, Job::ItemBulkImport.csv_file_location_root,
+  [CfsDirectory.export_root, Job::ItemBulkImport.csv_file_location_root,
    FitsResult.storage_root, Settings.medusa.cfs.fg_delete_holding].each do |path|
     Dir[File.join(path, '*')].each do |dir|
       FileUtils.rm_rf(dir) if File.exist?(dir)
@@ -20,6 +20,6 @@ Before do
     end
   end
 
-  Application.storage_manager.main_root
+  Application.storage_manager.main_root.delete_all_content
 
 end
