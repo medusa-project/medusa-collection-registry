137a8dc5782b2eac64ada9b8d1d796c3598db481
Howard Ding <hding2@illinois.edu>
2018-07-31 16:45:13 -0500

    Add fits root, use new medusa_storage RootFactory

--- app/models/storage_manager.rb
+++ app/models/storage_manager.rb
@@ -1,19 +1,20 @@
 class StorageManager
 
-  attr_accessor :main_root, :amqp_roots, :project_staging_root, :accrual_roots, :tmpdir
+  attr_accessor :main_root, :amqp_roots, :project_staging_root, :accrual_roots,
+                :fits_root, :tmpdir
 
   def initialize
     initialize_main_storage
     initialize_amqp_storage
     initialize_project_staging_storage
     initialize_accrual_storage
+    initialize_fits_root
     initialize_tmpdir
   end
 
   def initialize_main_storage
     root_config = Settings.storage.main_root.to_h
-    root_set = MedusaStorage::RootSet.new(Array.wrap(root_config))
-    self.main_root = root_set.at(root_config[:name]) || raise('Main storage root not defined')
+    self.main_root = MedusaStorage::RootFactory.create_root(root_config)
   end
 
   def initialize_amqp_storage
@@ -23,13 +24,17 @@ class StorageManager
 
   def initialize_project_staging_storage
     root_config = Settings.storage.project_staging.to_h
-    root_set = MedusaStorage::RootSet.new(Array.wrap(root_config))
-    self.project_staging_root = root_set.at(root_config[:name]) || raise('Project staging root not defined')
+    self.project_staging_root = MedusaStorage::RootFactory.create_root(root_config)
   end
 
   def initialize_accrual_storage
     accrual_config = Settings.storage.accrual.collect(&:to_h)
     self.accrual_roots = MedusaStorage::RootSet.new(accrual_config)
+  end
+
+  def initialize_fits_root
+    fits_config = Settings.storage.fits.to_h
+    self.fits_root = MedusaStorage::RootFactory.create_root(fits_config)
   end
 
   def amqp_root_at(name)
