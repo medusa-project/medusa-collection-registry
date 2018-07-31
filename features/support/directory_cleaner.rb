9ee479a8414ccfd9426f4ac2b3d229181d72691e
Howard Ding <hding2@illinois.edu>
2018-07-31 10:59:21 -0500

    Do some more deletion before tests

--- features/support/directory_cleaner.rb
+++ features/support/directory_cleaner.rb
@@ -11,6 +11,8 @@ Before do
   end
 
   Application.storage_manager.main_root.delete_all_content
+  Application.storage_manager.project_staging_root.delete_all_content
   Application.storage_manager.accrual_roots.all_roots.each {|root| root.delete_all_content}
-  
+  Application.storage_manager.amqp_roots.all_roots.each {|root| root.delete_all_content}
+
 end
