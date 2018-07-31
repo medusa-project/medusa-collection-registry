cb3f9d569dbd437e38cf6fbb3901980ba72ef212
Howard Ding <hding2@illinois.edu>
2018-07-31 16:44:43 -0500

    update for fits storage using medusa_storage

--- features/support/directory_cleaner.rb
+++ features/support/directory_cleaner.rb
@@ -3,8 +3,7 @@ require 'fileutils'
 #before each test make sure that the cfs directories, any staging directories, item_upload_csv directories are empty
 Before do
 
-  [CfsDirectory.export_root, Job::ItemBulkImport.csv_file_location_root,
-   FitsResult.storage_root, Settings.medusa.cfs.fg_delete_holding].each do |path|
+  [CfsDirectory.export_root, Job::ItemBulkImport.csv_file_location_root, Settings.medusa.cfs.fg_delete_holding].each do |path|
     Dir[File.join(path, '*')].each do |dir|
       FileUtils.rm_rf(dir) if File.exist?(dir)
     end
@@ -14,5 +13,6 @@ Before do
   Application.storage_manager.project_staging_root.delete_all_content
   Application.storage_manager.accrual_roots.all_roots.each {|root| root.delete_all_content}
   Application.storage_manager.amqp_roots.all_roots.each {|root| root.delete_all_content}
+  Application.storage_manager.fits_root.delete_all_content
 
 end
