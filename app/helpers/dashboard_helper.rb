3a9e0f616fde43d7a6fe86a6cbd8e2913e48d2d9
Howard Ding <hding2@illinois.edu>
2018-08-13 17:03:11 -0500

    remove unused code

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -7,33 +7,6 @@ module DashboardHelper
     tabs
   end
 
-  # File stats for objects preservation
-
-  def list_of_object_file_formats
-    # return list of unique content types
-    []
-  end
-
-  def size_objects_type_format (ct)
-    # only active records of certain format that have been ingested
-    number_with_precision(0.0, precision: 4)
-  end
-
-  def file_count_objects_type_format (ct)
-    # only active records of certain format that have been ingested
-    0
-  end
-
-  def size_objects_total
-    # only active records that have been ingested
-    0
-  end
-
-  def file_count_objects_total
-    # only active records that have been ingested
-    0
-  end
-
   INGEST_STATE_LABELS = {start: 'Starting', copying: 'Copying from staging', amazon_backup: 'Backing up to Amazon',
                          end: 'Ended'}
 
