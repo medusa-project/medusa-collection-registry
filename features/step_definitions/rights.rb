5cae6bcd58af08ed5734abaddf4c604f31763bea
Howard Ding <hding2@illinois.edu>
2018-08-13 15:09:09 -0500

    remove unused code

--- features/step_definitions/rights.rb
+++ features/step_definitions/rights.rb
@@ -9,23 +9,3 @@ end
 Then /^I should see the rights declaration section$/ do
   page.should have_selector('#rights-declaration')
 end
-
-And(/^the (.*) with (.*) '([^']*)' has (.*) rights$/) do |object_type, key, value, rights_type|
-  instance = find_object(object_type, key, value)
-  rights_declaration =
-      if instance.respond_to?(:rights_declaration)
-        instance.rights_declaration
-      elsif instance.respond_to?(:file_group)
-        instance.file_group.rights_declaration
-      end
-  rights_declaration.access_restrictions =
-      case rights_type
-        when 'public'
-          'DISSEMINATE'
-        when 'private'
-          'DISSEMINATE/DISALLOW'
-        else
-          raise Runtime Error, 'Unrecognized rights type'
-      end
-  rights_declaration.save!
-end
