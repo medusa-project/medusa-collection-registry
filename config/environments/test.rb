75e1418a23676dc926d0477a3614ce37835cd378
Howard Ding <hding2@illinois.edu>
2018-08-14 10:42:54 -0500

    Eager load code in the test environment. This is not critical, but I think coverage was missing files because they were never loaded.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,7 +29,7 @@ MedusaCollectionRegistry::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 
-  config.eager_load = false
+  config.eager_load = true
   config.cache_store = :dalli_store
 
   config.colorize_logging = false
