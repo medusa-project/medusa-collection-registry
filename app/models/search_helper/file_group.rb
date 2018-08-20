a10bc0220202668916c5c73f5777a1811e717ab8
Howard Ding <hding2@illinois.edu>
2018-08-20 13:15:18 -0500

    remove external id from search helper for file group

--- app/models/search_helper/file_group.rb
+++ app/models/search_helper/file_group.rb
@@ -6,8 +6,7 @@ class SearchHelper::FileGroup < SearchHelper::SearchBase
 
   def columns
     [{header: 'Title', solr_field: :title, value_method: :search_file_group_link, searchable: true},
-     {header: 'Description', solr_field: :description, value_method: :description, searchable: true},
-     {header: 'External ID', solr_field: :external_id, value_method: :external_id, searchable: true}]
+     {header: 'Description', solr_field: :description, value_method: :description, searchable: true}]
   end
 
 end
