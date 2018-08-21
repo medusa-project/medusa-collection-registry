b1fd35a316946b67a08b7b59bbf0a4613c830fbe
Howard Ding <hding2@illinois.edu>
2018-08-21 11:43:27 -0500

    These drops are failing for some reason on staging, so retry with a bit different syntax.

--- db/migrate/20180820200518_remove_package_profile_trigger.rb
+++ db/migrate/20180820200518_remove_package_profile_trigger.rb
@@ -1,6 +1,6 @@
 class RemovePackageProfileTrigger < ActiveRecord::Migration[5.1]
   def change
-    ActiveRecord::Base.connection.execute('DROP TRIGGER file_groups_touch_package_profile_trigger ON file_groups')
-    ActiveRecord::Base.connection.execute('DROP FUNCTION public.file_groups_touch_package_profile')
+    ActiveRecord::Base.connection.execute('DROP TRIGGER file_groups_touch_package_profile_trigger ON file_groups ;')
+    ActiveRecord::Base.connection.execute('DROP FUNCTION public.file_groups_touch_package_profile ;')
   end
 end
