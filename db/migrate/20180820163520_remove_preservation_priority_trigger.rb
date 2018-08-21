b37e2314642dcb777e9fd8240603f10037003735
Howard Ding <hding2@illinois.edu>
2018-08-21 11:49:29 -0500

    It's not imperative to remove the functions, just the triggers, so let's try that.

--- db/migrate/20180820163520_remove_preservation_priority_trigger.rb
+++ db/migrate/20180820163520_remove_preservation_priority_trigger.rb
@@ -1,6 +1,6 @@
 class RemovePreservationPriorityTrigger < ActiveRecord::Migration[5.1]
   def change
     ActiveRecord::Base.connection.execute('DROP TRIGGER collections_touch_preservation_priority_trigger ON collections ;')
-    ActiveRecord::Base.connection.execute('DROP FUNCTION collections_touch_preservation_priority ;')
+    #ActiveRecord::Base.connection.execute('DROP FUNCTION collections_touch_preservation_priority ;')
   end
 end
