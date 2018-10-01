83dfb7d752d5797a3b039e688bcc58eb483449db
Howard Ding <hding2@illinois.edu>
2018-10-01 13:28:16 -0500

    Link directories relevant for webpacker

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :linked_files, %w(config/database.yml config/sunspot.yml config/settings/pro
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle',
-                                               'public/system', 'tmp/item_upload_csv')
+                                               'public/system', 'tmp/item_upload_csv', 'public/packs', 'node_modules')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
