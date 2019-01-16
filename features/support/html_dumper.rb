2382822cd616442f45a29f135f919de30059fbb0
Howard Ding <hding2@illinois.edu>
2019-01-16 09:36:49 -0600

    Try to make it easier to activate/deactivate html dumper

--- features/support/html_dumper.rb
+++ features/support/html_dumper.rb
@@ -6,7 +6,7 @@ require 'singleton'
 class HtmlDumper
   include Singleton
 
-  attr_accessor :dump_number
+  attr_accessor :dump_number, :active
 
   def initialize()
     FileUtils.rm_rf(dump_dir)
@@ -14,11 +14,16 @@ class HtmlDumper
     self.dump_number = 0
   end
 
+  def activate
+    self.active = true
+  end
+
   def dump_dir
     @dump_dir ||= File.join(Rails.root, 'tmp', 'html_dump')
   end
 
   def dump(page)
+    return unless self.active
     if page.present? and page.html.start_with?('<!DOCTYPE html>')
       self.dump_number += 1
       target_file = File.join(dump_dir, "#{dump_number}.html")
