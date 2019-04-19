4e0305e33ed81213a40197f0d41c30b4ae1c8fbe
Howard Ding <hding2@illinois.edu>
2019-04-19 11:44:08 -0500

    Trying to figure out problem with attachments.

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -3,7 +3,7 @@ class ReportMailer < MedusaBaseMailer
   def cfs_directory_map(job, report_io)
     @job = job
     subject("Medusa Report Map for #{job.cfs_directory.relative_path}")
-    attachments['report.txt'] = report_io.string
+    mail.attachments['report.txt'] = report_io.string
     mail(to: job.user.email)
   end
 
