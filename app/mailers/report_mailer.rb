502374b8c69d64d00240545710b6f031e49bce07
Howard Ding <hding2@illinois.edu>
2019-04-19 11:51:59 -0500

    revert to previous version

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -3,7 +3,7 @@ class ReportMailer < MedusaBaseMailer
   def cfs_directory_map(job, report_io)
     @job = job
     subject("Medusa Report Map for #{job.cfs_directory.relative_path}")
-    attachments['report.txt'] = {content: report_io.string, mime_type: 'text/x-diff'}
+    attachments['report.txt'] = report_io.string
     mail(to: job.user.email)
   end
 
