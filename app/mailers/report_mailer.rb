7172afce8030b2206702ecf612ebf6fb7b8d642f
Howard Ding <hding2@illinois.edu>
2019-04-19 12:16:55 -0500

    Try putting attachments after mail

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -10,11 +10,11 @@ class ReportMailer < MedusaBaseMailer
   def cfs_directory_manifest(job, report_io)
     @job = job
     subject("Medusa Report Manifest for #{job.cfs_directory.relative_path}")
-    attachments['report.tsv'] = {content: report_io.string, mime_type: 'text/tab-separated-values'}
     mail(to: job.user.email) do |format|
       format.text
       format.html
     end
+    attachments['report.tsv'] = {content: report_io.string, mime_type: 'text/tab-separated-values'}
   end
 
 end
