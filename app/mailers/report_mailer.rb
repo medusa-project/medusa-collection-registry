f071c716a9b821cc52424e2ba6275771b8c7832a
Howard Ding <hding2@illinois.edu>
2019-04-19 12:59:39 -0500

    undo subject call

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -2,14 +2,14 @@ class ReportMailer < MedusaBaseMailer
 
   def cfs_directory_map(job, report_io)
     @job = job
-    @subject = subject("Medusa cfs directory map")
+    @subject = "Medusa cfs directory map"
     attachments['report.txt'] = report_io.string
     mail(to: job.user.email)
   end
 
   def cfs_directory_manifest(job, report_io)
     @job = job
-    @subject = subject("Medusa cfs directory manifest")
+    @subject = "Medusa cfs directory manifest"
     attachments['report.tsv'] = report_io.string
     mail(to: job.user.email)
   end
