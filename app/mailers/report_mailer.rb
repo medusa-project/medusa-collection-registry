ec21ada2730723f3da9be8ea1d029c5453221bcd
Howard Ding <hding2@illinois.edu>
2019-04-19 12:36:27 -0500

    Maybe the mail call in add_subject is bad?

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -2,20 +2,14 @@ class ReportMailer < MedusaBaseMailer
 
   def cfs_directory_map(job, report_io)
     @job = job
-    subject("Medusa Report Map for #{job.cfs_directory.relative_path}")
     attachments['report.txt'] = report_io.string
-    mail(to: job.user.email)
+    mail(to: job.user.email, subject: subject("Medusa Report Map for #{job.cfs_directory.relative_path}"))
   end
 
   def cfs_directory_manifest(job, report_io)
     @job = job
-    subject("Medusa Report Manifest for #{job.cfs_directory.relative_path}")
-    mail.attachments['report.tsv'] = {content: report_io.string, mime_type: 'text/tab-separated-values'}
-    mail(to: job.user.email) do |format|
-      format.text
-      format.html
-    end
-
+    attachments['report.tsv'] = {content: report_io.string, mime_type: 'text/tab-separated-values'}
+    mail(to: job.user.email, subject: subject("Medusa Report Manifest for #{job.cfs_directory.relative_path}"))
   end
 
 end
