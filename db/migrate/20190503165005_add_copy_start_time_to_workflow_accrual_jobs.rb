d2bbaf0780ec143cb401aa1d12e97bddbd97963d
Howard Ding <hding2@illinois.edu>
2019-05-03 14:30:02 -0500

    Make these so that they'll work if there are existing Workflow::AccrualJobs

--- db/migrate/20190503165005_add_copy_start_time_to_workflow_accrual_jobs.rb
+++ db/migrate/20190503165005_add_copy_start_time_to_workflow_accrual_jobs.rb
@@ -1,5 +1,9 @@
 class AddCopyStartTimeToWorkflowAccrualJobs < ActiveRecord::Migration[5.2]
   def change
     add_column :workflow_accrual_jobs, :copy_start_time, :datetime
+    Workflow::AccrualJob.all.each do |wf|
+      wf.copy_start_time = Time.now
+      wf.save
+    end
   end
 end
