bcc6bd3f41c67e8e941f67c66f1c3b619c05232f
Howard Ding <hding2@illinois.edu>
2019-06-20 12:12:11 -0500

    Add tests for coverage

--- test/unit/models/cfs_directory_test.rb
+++ test/unit/models/cfs_directory_test.rb
@@ -16,7 +16,7 @@ class CfsDirectoryTest < ActiveSupport::TestCase
     assert_equal expected_ids, parent_dir.recursive_subdirectory_ids.sort
 
     assert_equal parent_tree.to_set, parent_dir.directories_in_tree.to_set
-    assert_equal [child_dirs, grandchild_dirs].flatten.to_set, parent_dir.directories_in_tree(false).to_set
+    assert_equal [child_dirs, grandchild_dirs].flatten.to_set, parent_dir.directories_in_tree(include_self: false).to_set
 
     relative_dirs = [aunt_dir, cousin_dirs].flatten.to_set
     aunt_dir.each_directory_in_tree do |dir|
@@ -24,6 +24,36 @@ class CfsDirectoryTest < ActiveSupport::TestCase
     end
     assert_equal [].to_set, relative_dirs
   end
+
+  test 'root cfs directories may not have the same path' do
+    FactoryBot.create(:attached_cfs_directory, path: '1/2')
+    other_file_group = FactoryBot.create(:bit_level_file_group)
+    bad_root_directory =  FactoryBot.build(:cfs_directory, parent: other_file_group, path: '1/2')
+    refute bad_root_directory.valid?
+    assert_equal 1, bad_root_directory.errors.size
+    assert_includes bad_root_directory.errors.full_messages, 'Path must be unique for roots'
+  end
+
+  test 'create file with specified components' do
+    directory = FactoryBot.create(:attached_cfs_directory, path: '1/2')
+    directory.send(:ensure_file_with_directory_components, 'file.txt', ['.', 'subdir', 'path'])
+    created_file = CfsFile.find_by(name: 'file.txt')
+    assert created_file
+    assert_equal 'path', created_file.cfs_directory.path
+    assert_equal 'subdir', created_file.cfs_directory.parent.path
+    assert_equal directory, created_file.cfs_directory.parent.parent
+  end
+
+  test 'create directory with specified components' do
+    directory = FactoryBot.create(:attached_cfs_directory, path: '1/2')
+    directory.send(:ensure_directory_with_directory_components, ['.', 'subdir', 'path'])
+    created_directory = CfsDirectory.find_by(path: 'path')
+    assert created_directory
+    assert_equal 'subdir', created_directory.parent.path
+    assert_equal directory, created_directory.parent.parent
+  end
+
+  private
 
   def create_child(parent)
     FactoryBot.create(:cfs_directory, parent: parent, root_cfs_directory: parent.root_cfs_directory)
