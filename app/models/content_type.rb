e0323ce51b225f2bd3d0c76836b00194caeb74c0
Howard Ding <hding2@illinois.edu>
2019-06-20 16:14:10 -0500

    Remove unneeded option

--- app/models/content_type.rb
+++ app/models/content_type.rb
@@ -1,7 +1,7 @@
 class ContentType < ApplicationRecord
   include RandomCfsFile
 
-  validates_uniqueness_of :name, allow_blank: nil
+  validates_uniqueness_of :name
   has_many :file_format_profiles_content_types_joins, dependent: :destroy
   has_many :file_format_profiles, through: :file_format_profiles_content_types_joins
   validates_numericality_of :cfs_file_count, :cfs_file_size
