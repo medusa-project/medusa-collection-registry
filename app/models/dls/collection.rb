c80c440e096d2dc071f885a688b5d45378452546
Howard Ding <hding2@illinois.edu>
2019-07-15 16:00:09 -0500

    finish hooking up new system to go to DLS from CR.

--- app/models/dls/collection.rb
+++ app/models/dls/collection.rb
@@ -14,4 +14,15 @@ class Dls::Collection
     collection_url + '/items'
   end
 
+  def best_url
+    return items_url if HTTParty.head(items_url, timeout: 3).success?
+    collection_response = HTTParty.head(collection_url, timeout: 3)
+    #if the collection is there and public this should succeed; if there and not public then should
+    # give an auth problem 403, and if not there I'm seeing a 500.
+    return collection_url if (collection_response.success? or (403 == collection_response.code))
+    return nil
+  rescue
+    return nil
+  end
+
 end
