0eee7877850bb73f38aeb8cf93dd3feb2da98f34
Howard Ding <hding2@illinois.edu>
2019-07-15 16:04:25 -0500

    Thread one of the calls to dls so that they can run concurrently.

--- app/models/dls/collection.rb
+++ app/models/dls/collection.rb
@@ -15,8 +15,13 @@ class Dls::Collection
   end
 
   def best_url
+    #This is just so we can do these two in parallel, even though we might not need this one.
+    collection_response_thread = Thread.new do
+      HTTParty.head(collection_url, timeout: 3)
+    end
     return items_url if HTTParty.head(items_url, timeout: 3).success?
-    collection_response = HTTParty.head(collection_url, timeout: 3)
+    collection_response_thread.join
+    collection_response = collection_response_thread.value
     #if the collection is there and public this should succeed; if there and not public then should
     # give an auth problem 403, and if not there I'm seeing a 500.
     return collection_url if (collection_response.success? or (403 == collection_response.code))
