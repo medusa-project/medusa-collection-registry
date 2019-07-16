e2101974250c25ab1417cde490eed88ff4c93073
Howard Ding <hding2@illinois.edu>
2019-07-16 13:58:33 -0500

    Try to make it clearer to users that they can't accrue to directories that already have an accrual. Remove the 'Add Files' links (and replace with some text, though I'm not fully happy with that). If one is submitted then put in a notice error.

--- app/controllers/accruals_controller.rb
+++ app/controllers/accruals_controller.rb
@@ -16,14 +16,19 @@ class AccrualsController < ApplicationController
   def submit
     cfs_directory = CfsDirectory.find params[:accrual][:cfs_directory_id]
     authorize! :accrue, cfs_directory
-    staging_path = params[:accrual][:staging_path]
-    clean_staging_path = Nokogiri::HTML.parse(staging_path).text
-    accrual_directories = params[:accrual][:accrual_directories].select {|d| d.present?}
-    accrual_files = params[:accrual][:accrual_files].select {|f| f.present?}
-    allow_overwrite = params[:accrual][:allow_overwrite]
-    Workflow::AccrualJob.create_for(current_user, cfs_directory, clean_staging_path, accrual_files, accrual_directories, allow_overwrite)
-    flash[:notice] = submission_notice
-    redirect_to cfs_directory
+    if Accrual.new(cfs_directory: cfs_directory).directory_accruable?
+      staging_path = params[:accrual][:staging_path]
+      clean_staging_path = Nokogiri::HTML.parse(staging_path).text
+      accrual_directories = params[:accrual][:accrual_directories].select {|d| d.present?}
+      accrual_files = params[:accrual][:accrual_files].select {|f| f.present?}
+      allow_overwrite = params[:accrual][:allow_overwrite]
+      Workflow::AccrualJob.create_for(current_user, cfs_directory, clean_staging_path, accrual_files, accrual_directories, allow_overwrite)
+      flash[:notice] = submission_notice
+      redirect_to cfs_directory
+    else
+      flash[:notice] = "This directory already has an accrual pending. Please wait for that accrual to finish before beginning another."
+      redirect_to cfs_directory
+    end
   end
 
   protected
