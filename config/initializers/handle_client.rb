6f44b9d3297dfae6913d5b82e9023afa6f8b074e
Colleen Fallaw <mfall3@illinois.edu>
2019-09-20 14:49:32 -0500

    remove temporary debug logging

--- config/initializers/handle_client.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'config'
-app = MedusaCollectionRegistry::Application
-app.medusa_host = nil
-app.handle_client = nil
-if Settings.handle_client.present?
-  app.medusa_host = Settings.handle_client.medusa_host
-  app.handle_client = HandleServer::URLClient.new(Settings.handle_client.handle_server.to_h.symbolize_keys)
-end
