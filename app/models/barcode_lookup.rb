07a3a59dee768a775ca3872645a6139754657293
Colleen Fallaw <mfall3@illinois.edu>
2020-06-15 17:04:44 -0500

    refine barcode lookup

--- app/models/barcode_lookup.rb
+++ app/models/barcode_lookup.rb
@@ -7,6 +7,7 @@ require 'alma_api/error_response'
 class BarcodeLookup < Object
   attr_accessor :barcode, :lookup_doc, :call_number
   @@alma_api = AlmaApi::Batch::ApiCaller.new(Settings.alma.host, Settings.alma.key)
+
   def initialize(barcode)
     self.barcode = barcode
     fetch_and_parse
@@ -19,97 +20,44 @@ class BarcodeLookup < Object
   def item_hashes
     h = []
     return h unless valid?
-    lookup_doc.xpath('item/bib_data').each do |bib_data_node|
-      h[:title] = begin
-                    bib_data_node.xpath('title').text
-                  rescue StandardError
-                    ''
-                  end
-      h[:author] = begin
-                     bib_data_node.xpath('author').text
-                   rescue StandardError
-                     ''
-                   end
-      h[:bib_id] = begin
-                     bib_data_node.xpath('mms_id').text
-                   rescue StandardError
-                     ''
-                   end
-      h[:imprint] = begin
-                      'placeholder imprint'
-                    rescue StandardError
-                      ''
-                    end
-      h[:oclc_number] = extract_oclc_number(bib_mfhd)
-      h[:call_number] = call_number
 
-      # TitleBrief,
-      puts "Title: #{bib_data_node.xpath('title').text}"
-
-      # Author,
-      puts "Author: #{bib_data_node.xpath('author').text}"
-
-      # BibId,
-      puts "mms_id (Alma bib_id): #{bib_data_node.xpath('mms_id').text}"
-
-      # not sure what imprint is here, so will look up later...
-      # probably 260 field in marc, which would be found by using
-      # place_of_publication, date_of_publication, and publisher_const
-
-      puts 'Network numbers:'
-      bib_data_node.xpath('network_numbers/network_number').each do |network_num_node|
-        puts ' ' * 2 + network_num_node.text
-      end
-
-    end
-
-    puts 'Call Numbers:'
-    lookup_doc.xpath('/item/holding_data/call_number').each do |call_number_node|
-      puts ' ' * 2 + call_number_node.text
-    end
+    bib_data_node = lookup_doc.xpath('item/bib_data')
+    h[:title] = safe_node_xpath_text(bib_data_node, 'title')
+    h[:author] = safe_node_xpath_text(bib_data_node, 'author')
+    h[:bib_id] = safe_node_xpath_text(bib_data_node, 'mms_id')
+    h[:imprint] = safe_node_xpath_text(lookup_doc, 'item/item_data/imprint')
+    h[:oclc_number] = oclc_number
+    h[:call_number] = safe_node_xpath_text(lookup_doc, 'item/holding_data/call_number')
+    h
   end
-
-  # def item_hashes
-  #   if self.valid?
-  #     self.bib_mfhds.collect do |bib_mfhd|
-  #       Hash.new.tap do |h|
-  #         h[:title] = bib_mfhd.at_css('TitleBrief').text.sub(/\s*\/\s*$/, '').strip rescue ''
-  #         h[:author] = bib_mfhd.at_css('Author').text.strip rescue ''
-  #         h[:bib_id] = bib_mfhd.at_css('BibId').text.strip rescue ''
-  #         h[:imprint] = bib_mfhd.at_css('Imprint').text.strip rescue ''
-  #         h[:oclc_number] = extract_oclc_number(bib_mfhd)
-  #         h[:call_number] = call_number
-  #       end
-  #     end
-  #   else
-  #     Array.new
-  #   end
-  # end
 
   protected
 
   def fetch_and_parse
     item_records = '/almaws/v1/items'
-    options = { 'item_barcode' => barcode }
+    options = {:item_barcode => barcode, :view => 'label'}
     lookup_doc_response = @api.get(item_records, options)
     self.lookup_doc = Nokogiri::XML(lookup_doc_response.body)
   rescue OpenURI::HTTPError
     self.lookup_doc = nil
   end
 
-
-  def call_number
-    arr = Array.new
-    self.lookup_doc.xpath('/item/holding_data/call_number').each do |call_number_node|
-      arr << call_number_node.text
-    end
-    arr.join(", ")
+  def safe_node_xpath_text(node, xpath)
+    node.xpath(xpath).text
   rescue StandardError
     ''
   end
 
-  def extract_oclc_number(bib_mfhd)
-    node = bib_mfhd.css('NetworkNumber').detect { |x| x.text.match(/^\(OCoLC\)/) }
-    node.text.sub(/^\(OCoLC\)/, '').strip if node
+  def oclc_number
+    network_numbers = []
+    bib_data_node.xpath('network_numbers/network_number').each do |network_num_node|
+      network_numbers << network_num_node.text
+    end
+    oclc_numbers = network_numbers.select { |x| x.match(/^\(OCoLC\)/) }
+    return '' if oclc_numbers.empty?
+
+    oclc_numbers.first.sub(/^\(OCoLC\)/, '').strip
+  rescue StandardError
+    ''
   end
 end
--- lib/tasks/alma.rake
+++ lib/tasks/alma.rake
@@ -13,11 +13,28 @@ namespace :alma do
     @alma = AlmaApi::Batch::ApiCaller.new(host,key)
     barcode = '25279'
     item_records = '/almaws/v1/items'
-    options = {'item_barcode' => barcode }
+    options = {:item_barcode => barcode }
     items_xml_response = @alma.get(item_records, options)
     lookup_doc = Nokogiri::XML(items_xml_response.body)
     puts lookup_doc
-    puts "\n******\n"
+    puts "\n****** bib_data\n"
     puts lookup_doc.xpath('item/bib_data')
+  end
+  desc 'howdy'
+  task howdy: :environment do
+    puts 'howdy'
+    host = Settings.alma.host
+    key = Settings.alma.key
+    puts host
+    puts key
+    @alma = AlmaApi::Batch::ApiCaller.new(host,key)
+    barcode = '25279'
+    item_records = '/almaws/v1/items'
+    options = {:item_barcode => barcode, :view => 'label' }
+    items_xml_response = @alma.get(item_records, options)
+    lookup_doc = Nokogiri::XML(items_xml_response.body)
+    #puts lookup_doc
+    puts "\n****** imprint\n"
+    puts lookup_doc.xpath('item/item_data/imprint').text
   end
 end
