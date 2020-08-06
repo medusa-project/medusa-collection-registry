693a5bf04d11c1bb5e994505cafa3a092fc90a94
Colleen Fallaw <mfall3@illinois.edu>
2020-08-06 10:39:05 -0500

    handle encoding issues in text preview

--- app/helpers/cfs_files_helper.rb
+++ app/helpers/cfs_files_helper.rb
@@ -1,11 +1,28 @@
-module CfsFilesHelper
+# frozen_string_literal: true
+require 'mime/types'
 
+module CfsFilesHelper
   module_function
 
   def text_preview(cfs_file)
+    candidate_string = raw_text_preview
+    if candidate_string.encoding == Encoding::ASCII_8BIT
+      candidate_string.force_encoding(Encoding::UTF_8)
+    end
+    unless candidate_string.encoding == Encoding::UTF_8
+      candidate_string.encode('UTF-8', invalid: :replace, undef: :replace)
+    end
+    candidate_string
+  rescue StandardError
+    'error encoding text preview'
+  end
+
+  def raw_text_preview
     cfs_file.with_input_io do |io|
       io.readline(nil, 500)
     end
+  rescue StandardError
+    'error getting text preview'
   end
 
   def preview_view(cfs_file)
@@ -16,5 +33,4 @@ module CfsFilesHelper
                   end
     "preview_viewer_#{viewer_type}"
   end
-
 end
