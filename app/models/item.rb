0a45f9b060fbf31984b5409f87eb43cbb10d81d4
Colleen Fallaw <mfall3@illinois.edu>
2021-08-18 14:49:26 -0500

    index new project item fields related to restricted access

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,7 +22,7 @@
     end
     %i(some_title bib_id call_number title author record_series_id oclc_number imprint local_title local_description
 reformatting_operator archival_management_system_url series sub_series box folder item_title creator date
-rights_information status equipment unique_identifier item_number source_media).each do |field|
+rights_information status equipment unique_identifier item_number source_media requester_info).each do |field|
       text field
       string field
     end
@@ -37,6 +37,9 @@ rights_information status equipment unique_identifier item_number source_media).
     string :project_title
     integer :project_id
     time :updated_at
+    string :ebook_status
+    string :external_link
+    string :reviewed_by
   end
 
   def self.uningested
