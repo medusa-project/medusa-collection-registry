969137e1f2f8ea1ba251310e3b8b06a26796c6f0
Colleen Fallaw <mfall3@illinois.edu>
2022-07-15 16:10:15 -0500

    undo problematic attempt to add timestamp to logger

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-class Logger
-  def format_message(severity, timestamp, progname, msg)
-    "#{timestamp} (#{$$}) #{msg}\n"
-  end
-end
