4b3f712175d66a21fa3c012915eb26522f9af3ed
Colleen Fallaw <mfall3@illinois.edu>
2022-07-15 16:00:55 -0500

    add timestamp to logging

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+class Logger
+  def format_message(severity, timestamp, progname, msg)
+    "#{timestamp} (#{$$}) #{msg}\n"
+  end
+end
