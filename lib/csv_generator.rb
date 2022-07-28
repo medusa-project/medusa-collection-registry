331ca1f141b276efd03bd33b55a1ccebb0c36221
Colleen Fallaw <mfall3@illinois.edu>
2022-07-28 11:51:41 -0500

    refine CSV update from ruby 2 to 3

--- lib/csv_generator.rb
+++ lib/csv_generator.rb
@@ -7,8 +7,8 @@ module CsvGenerator
   #The hash keys are methods to call on the members of the collection to get the csv values, with '' on any error;
   #the hash values are the headers
   #csv options are passed directly to CSV
-  def generate(collection, header_spec, csv_options = CSV::DEFAULT_OPTIONS)
-    CSV.generate(csv_options) do |csv|
+  def generate(collection, header_spec, csv_options = {} )
+    CSV.generate do |csv|
       csv << header_spec.values
       collection.find_each(batch_size: 100) do |record|
         record = record.decorate if record.decorator_class?
